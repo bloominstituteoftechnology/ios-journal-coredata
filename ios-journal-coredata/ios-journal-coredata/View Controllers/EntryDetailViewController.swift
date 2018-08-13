93fd3c191957db31a67d7e97ed57b8165abc4db0
Conner Alegre <cga0@pm.me>
2018-08-13 14:06:13 -0600

    setup EntryDetailViewController with appropriate outlets and Storyboard

---
+++ ios-journal-coredata/ios-journal-coredata/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EntryDetailViewController.swift
+//  ios-journal-coredata
+//
+//  Created by Conner on 8/13/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+}
