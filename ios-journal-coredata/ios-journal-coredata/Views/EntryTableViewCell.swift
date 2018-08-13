5e760fb1350ac30b0cb3f1c0bf7c4f8d4f96ada0
Conner Alegre <cga0@pm.me>
2018-08-13 13:59:48 -0600

    add custom cell view controller and appropriate outlets

---
+++ ios-journal-coredata/ios-journal-coredata/Views/EntryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EntryTableViewCell.swift
+//  ios-journal-coredata
+//
+//  Created by Conner on 8/13/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet var entryTitleLabel: UILabel!
+    @IBOutlet var entryTimeLabel: UILabel!
+    @IBOutlet var entryBodyLabel: UILabel!
+    
+}
