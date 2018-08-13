449b8aa6b68d85a75334da74d267d793bedd9b8b
Samantha Gatt <smfgatt@gmail.com>
2018-08-13 15:45:09 -0400

    Configures entry cell view

--- Journal/Journal/VCs/EntryTableViewCell.swift
+++ Journal/Journal/VCs/EntryTableViewCell.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
+    // MARK: - Functions
+    
+    func updateViews() {
+        guard let title = entry?.title,
+            let body = entry?.body,
+            let timestamp = entry?.timestamp else {
+                return
+        }
+        
+        titleLabel.text = title
+        bodyLabel.text = body
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        let date = dateFormatter.string(from: timestamp)
+        
+        dateLabel.text = date
+    }
+    
+    
+    // MARK: - Properties
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
     // MARK: - Outlets
 
     @IBOutlet weak var titleLabel: UILabel!
