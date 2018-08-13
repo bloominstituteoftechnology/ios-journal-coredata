08b87b9e94d254ba781856b74ca12ade27eab5c7
Samantha Gatt <smfgatt@gmail.com>
2018-08-13 15:35:54 -0400

    Configures entry controller

--- Journal/Journal/Core Data/Entry+Convenience.swift
+++ Journal/Journal/Core Data/Entry+Convenience.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreData
 
 extension Entry {
-    convenience init(title: String, body: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, context: NSManagedObjectContext) {
+    convenience init(title: String, body: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.body = body
---
+++ Journal/Journal/Core Data/EntryController.swift
@@ -0,0 +1,64 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Samantha Gatt on 8/13/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+    // MARK: - Properties
+    
+    var entries: [Entry] {
+        return loadFromPersistentStore()
+    }
+    
+    
+    // MARK: - CRUD
+    
+    func create(title: String, body: String) {
+        let _ = Entry(title: title, body: body)
+        saveToPersistentStore()
+    }
+    
+    func update(entry: Entry, title: String, body: String, timestamp: Date = Date()) {
+        entry.title = title
+        entry.body = body
+        entry.timestamp = timestamp
+        saveToPersistentStore()
+    }
+    
+    func delete(entry: Entry) {
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(entry)
+        saveToPersistentStore()
+    }
+    
+    
+    // MARK: - Persistence
+    
+    func saveToPersistentStore() {
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            try moc.save()
+        }
+        catch {
+            NSLog("Error saving entry: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() -> [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            return try moc.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching tasks: \(error)")
+            return []
+        }
+    }
+}
