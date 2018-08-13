e0138845632bf2b92055ddae40c99623b54e3697
Samantha Gatt <smfgatt@gmail.com>
2018-08-13 15:18:35 -0400

    Configures Entry model and a convenience initializer

---
+++ Journal/Journal/Core Data/Entry+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Samantha Gatt on 8/13/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String, body: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
