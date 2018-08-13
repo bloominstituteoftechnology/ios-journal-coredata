af659cae09a60845cec0b0e970e0ab4f851ef35d
Linh Bouniol <paulalinh89@gmail.com>
2018-08-13 12:28:27 -0700

    Created storyboards

---
+++ Journal (Core Data)/Journal (Core Data)/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryDetailViewController.swift
+//  Journal (Core Data)
+//
+//  Created by Linh Bouniol on 8/13/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ Journal (Core Data)/Journal (Core Data)/View/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  Journal (Core Data)
+//
+//  Created by Linh Bouniol on 8/13/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bodyTextLabel: UILabel!
+    @IBOutlet var timestampLabel: UILabel!
+    
+}
