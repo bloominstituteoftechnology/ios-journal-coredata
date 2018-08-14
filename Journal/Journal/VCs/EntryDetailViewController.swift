c9e25ee6b4d7f0ffe544861c16b6844b3d4a06d2
Samantha Gatt <smfgatt@gmail.com>
2018-08-14 14:56:12 -0400

    Updates save action and update views function in entry detail vc

--- Journal/Journal/VCs/EntryDetailViewController.swift
+++ Journal/Journal/VCs/EntryDetailViewController.swift
@@ -24,6 +24,10 @@ class EntryDetailViewController: UIViewController {
         navigationController?.title = entry?.title ?? "Create Entry"
         titleTextField.text = entry?.title ?? ""
         bodyTextView.text = entry?.body ?? ""
+        
+        guard let entryMoodRawValue = EntryMood(rawValue: entry?.mood ?? "😐"),
+            let entryMoodIndex = EntryMood.moods.index(of: entryMoodRawValue) else { return }
+        moodSegmentedControl.selectedSegmentIndex = entryMoodIndex
     }
     
     
@@ -32,11 +36,12 @@ class EntryDetailViewController: UIViewController {
     @IBAction func save(_ sender: Any) {
         guard let title = titleTextField.text, !title.isEmpty else { return }
         let body = bodyTextView.text
+        let mood = EntryMood.moods[moodSegmentedControl.selectedSegmentIndex]
         
         if let thisEntry = entry {
-            entryController?.update(entry: thisEntry, title: title, body: body)
+            entryController?.update(entry: thisEntry, title: title, body: body, mood: mood)
         } else {
-            entryController?.create(title: title, body: body)
+            entryController?.create(title: title, body: body, mood: mood)
         }
         
         entryController?.saveToPersistentStore()
