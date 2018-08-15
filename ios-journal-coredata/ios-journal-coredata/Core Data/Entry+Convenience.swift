8ff27cface9f1f374abe9b0467cd1ce037dae2d3
Conner Alegre <cga0@pm.me>
2018-08-15 15:44:01 -0600

    entry representation

--- ios-journal-coredata/ios-journal-coredata/Core Data/Entry+Convenience.swift
+++ ios-journal-coredata/ios-journal-coredata/Core Data/Entry+Convenience.swift
@@ -14,29 +14,42 @@ enum Mood: String {
     case sad = "☹️"
     case meh = "😐"
     case happy = "🙂"
-    
+
     static var allMoods: [Mood] {
         return [.sad, .meh, .happy]
     }
 }
 
 extension Entry {
-    
+
     convenience init(title: String,
-                     identifier: String,
-                     timestamp: Date,
-                     bodyText: String,
-                     mood: Mood = .meh,
-                     managedObjectContext: NSManagedObjectContext = CoreDataManager.shared.mainContext) {
-        
+        identifier: String,
+        timestamp: Date,
+        bodyText: String,
+        mood: Mood = .meh,
+        managedObjectContext: NSManagedObjectContext = CoreDataManager.shared.mainContext) {
+
         self.init(context: managedObjectContext)
-        
+
         self.title = title
         self.identifier = identifier
         self.timestamp = timestamp
         self.bodyText = bodyText
         self.mood = mood.rawValue
-        
+
     }
-    
+
+    convenience init?(entryRepresentation: EntryRepresentation,
+        managedObjectContext: NSManagedObjectContext = CoreDataManager.shared.mainContext) {
+
+        guard let mood = Mood(rawValue: entryRepresentation.mood) else { return nil }
+
+        self.init(title: entryRepresentation.title,
+            identifier: entryRepresentation.identifier,
+            timestamp: entryRepresentation.timestamp,
+            bodyText: entryRepresentation.bodyText,
+            mood: mood
+        )
+    }
+
 }
