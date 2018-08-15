2d9c9a52de2ce217f48cca9585415767c466938d
Carolyn Lea <carolynrlea@gmail.com>
2018-08-15 15:16:06 -0600

    Add EntryController changes

--- JournalWithCoreData/JournalWithCoreData/ModelController/EntryController.swift
+++ JournalWithCoreData/JournalWithCoreData/ModelController/EntryController.swift
@@ -9,9 +9,12 @@
 import Foundation
 import CoreData
 
+let baseURL = URL(string: "https://day3-journal.firebaseio.com/")!
+
 class EntryController
 {
     var entry: Entry?
+    typealias CompletionHandler = (Error?) -> Void
     
     func saveToPersistentStore()
     {
@@ -29,9 +32,10 @@ class EntryController
     
     func createEntry(title: String, bodyText: String, mood:EntryMood)
     {
-        let _ = Entry(title: title, bodyText: bodyText, mood: mood)
+        let entry = Entry(title: title, bodyText: bodyText, mood: mood)
         print(title, bodyText)
         saveToPersistentStore()
+        put(entry: entry)
     }
     
     func updateEntry(entry: Entry, title: String, bodyText: String, timestamp: NSDate = NSDate(), mood: EntryMood)
@@ -43,6 +47,7 @@ class EntryController
         entry.mood = mood.rawValue
         print(title, bodyText)
         saveToPersistentStore()
+        put(entry: entry)
     }
     
     func deleteEntry(entry: Entry)
@@ -50,7 +55,7 @@ class EntryController
         let entry = entry
         let moc = CoreDataStack.shared.mainContext
         moc.delete(entry)
-        
+        deleteEntryFromServer(entry: entry)
         do
         {
             try moc.save()
@@ -61,7 +66,49 @@ class EntryController
         }
     }
     
+    func deleteEntryFromServer(entry: Entry, completion: @escaping CompletionHandler = { _ in })
+    {
+        let requestURL = baseURL.appendingPathComponent(entry.identifier!).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error PUTing entry to server: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+            }
+            .resume()
+    }
     
+    func put(entry:Entry, completion: @escaping CompletionHandler = { _ in })
+    {
+        let requestURL = baseURL.appendingPathComponent(entry.identifier!).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"//to update existing task
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(entry)
+        } catch {
+            NSLog("Error encoding entry \(entry): \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error PUTing entry to server: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+        .resume()
+    }
     
     
     
