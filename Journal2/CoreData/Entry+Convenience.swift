ab7f126e466de4c0d322b4911c79ea1fcff7edd0
Carolyn Lea <carolynrlea@gmail.com>
2018-08-20 15:49:56 -0600

    Finish CoreDataStack. Finish Entry+Convenience

--- Journal2/CoreData/Entry+Convenience.swift
+++ Journal2/CoreData/Entry+Convenience.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import CoreData
+
+extension Entry
+{
+    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identitifier: String = UUID().uuidString, context: NSManagedObjectContext = CoreDataStack.shared.mainContext)
+    {
+        self.init(context: context)
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identitifier
+    }
+}
