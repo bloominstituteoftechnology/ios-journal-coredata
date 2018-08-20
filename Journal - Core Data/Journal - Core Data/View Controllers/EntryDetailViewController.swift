3491df41690af2ce3ca6ab1f5663182d769ed4cf
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-20 17:01:05 -0400

    Set up storyboards, actions, and outlets

---
+++ Journal - Core Data/Journal - Core Data/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryDetailViewController.swift
+//  Journal - Core Data
+//
+//  Created by Lisa Sampson on 8/20/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func saveButtonWasTapped(_ sender: Any) {
+        
+    }
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+}
---
+++ Journal - Core Data/Journal - Core Data/Views/EntryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EntryTableViewCell.swift
+//  Journal - Core Data
+//
+//  Created by Lisa Sampson on 8/20/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+}
