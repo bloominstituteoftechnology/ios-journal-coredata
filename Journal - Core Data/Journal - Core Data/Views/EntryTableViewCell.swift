39ee76b9f643f8475db6a22f1398f8e58345227b
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-20 18:18:52 -0400

    Create cell

--- Journal - Core Data/Journal - Core Data/Views/EntryTableViewCell.swift
+++ Journal - Core Data/Journal - Core Data/Views/EntryTableViewCell.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
+    func updateViews() {
+        guard let entry = entry else { return }
+        
+        titleLabel.text = entry.title
+        bodyTextLabel.text = entry.bodyText
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        timestampLabel.text = entry.timestamp.map({ (timestamp) -> String in
+            return dateFormatter.string(from: timestamp)
+        })
+    }
     
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyTextLabel: UILabel!
