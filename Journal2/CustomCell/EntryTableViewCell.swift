32b25f0215778c4a9b523aec0d6609a09a6f6fb6
Carolyn Lea <carolynrlea@gmail.com>
2018-08-20 16:28:31 -0600

    Finish EntryCell

--- Journal2/CustomCell/EntryTableViewCell.swift
+++ Journal2/CustomCell/EntryTableViewCell.swift
@@ -13,4 +13,28 @@ class EntryTableViewCell: UITableViewCell
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyTextLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
+    
+    var entry: Entry?
+    {
+        didSet
+        {
+            updateViews()
+        }
+    }
+    
+    private func updateViews()
+    {
+        if let entry = entry
+        {
+            let currentDate = entry.timestamp
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy, h:mm a"
+            let dateString = dateFormatter.string(from: currentDate!)
+            
+            titleLabel.text = entry.title
+            timestampLabel.text = dateString
+            bodyTextLabel.text = entry.bodyText
+        }
+    }
 }
