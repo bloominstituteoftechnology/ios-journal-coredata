adab3566e44b1948ff48d8eabe55cb533f8b3bae
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-09-17 17:18:53 -0500

    Created UI and blank Cocoa Touch classes

---
+++ Journal/Cells/EntryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Jason Modisett on 9/17/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    private func updateViews() {
+        
+        
+    }
+    
+    
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var entryTimestampLabel: UILabel!
+    @IBOutlet weak var entryContentLabel: UILabel!
+    
+}
---
+++ Journal/View Controllers/EntryDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDetailsViewController.swift
+//  Journal
+//
+//  Created by Jason Modisett on 9/17/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func updateViews() {
+        
+        
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        
+        
+    }
+
+    
+    
+    
+    @IBOutlet weak var entryTitleTextField: UITextField!
+    @IBOutlet weak var entryContentTextView: UITextView!
+
+}
