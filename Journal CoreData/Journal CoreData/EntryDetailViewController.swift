f2fa4edb122903f3d23f552cea8fda5e836cf3fb
Moin Uddin <U_Moin75@yahoo.com>
2018-09-17 18:23:31 -0400

    Add and Link cocoa touch files as well as outlets

---
+++ Journal CoreData/Journal CoreData/EntryDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryDetailViewController.swift
+//  Journal CoreData
+//
+//  Created by Moin Uddin on 9/17/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    @IBAction func saveEntry(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
