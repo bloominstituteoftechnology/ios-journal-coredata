b5797b2032ab411382004b7f5d13b9363be1f391
Daniela Parra <dyparra47@gmail.com>
2018-09-17 16:53:55 -0400

    Fix custom table cell label.

--- Journal/Journal/View Controllers/EntryDetailViewController.swift
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -13,19 +13,42 @@ class EntryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        if isViewLoaded {
+            guard let entry = entry else {
+                title = "Create Entry"
+                return
+            }
+            
+            title = entry.title
+            
+            nameTextField.text = entry.title
+            bodyTextView.text = entry.bodyText
+        }
     }
-    */
+    @IBAction func saveEntry(_ sender: Any) {
+        guard let title = nameTextField.text,
+            let bodyText = bodyTextView.text else { return }
+        
+        if let entry = entry {
+            entryController?.update(entry: entry, title: title, bodyText: bodyText)
+        } else {
+            entryController?.createEntry(with: title, bodyText: bodyText)
+        }
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    var entryController: EntryController?
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var bodyTextView: UITextView!
     
