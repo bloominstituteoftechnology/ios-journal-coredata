c72baf79ebd77d60de6430a7169a5c3371a01c5c
Moin Uddin <U_Moin75@yahoo.com>
2018-09-17 18:27:43 -0400

    Create CoreDataStack class file

---
+++ Journal CoreData/Journal CoreData/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  Journal CoreData
+//
+//  Created by Moin Uddin on 9/17/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Journal")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext  {
+        return container.viewContext
+    }
+}
