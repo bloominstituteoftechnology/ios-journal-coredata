b76c50f18484e3fbd51745a41f61c9ea8053edb8
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-17 14:16:13 -0600

    Lay out views and add view controllers

---
+++ Journal Core Data/Journal Core Data/View/EntryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryDetailViewController.swift
+//  Journal Core Data
+//
+//  Created by Dillon McElhinney on 9/17/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func saveEntry(_ sender: Any) {
+        
+    }
+}
---
+++ Journal Core Data/Journal Core Data/View/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  Journal Core Data
+//
+//  Created by Dillon McElhinney on 9/17/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+
+}
