3c61798c695152f9b64d8f28955114c165540d03
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-09-17 18:35:11 -0500

    Finished view controller implementations and completed project

--- Journal/Cells/EntryTableViewCell.swift
+++ Journal/Cells/EntryTableViewCell.swift
@@ -11,10 +11,19 @@ import UIKit
 class EntryTableViewCell: UITableViewCell {
     
     private func updateViews() {
+        guard let entry = entry else { return }
         
+        entryTitleLabel.text = entry.title
+        entryContentLabel.text = entry.bodyText
         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, h:mm a"
+        let timestampString = formatter.string(from: entry.timestamp ?? Date())
+        
+        entryTimestampLabel.text = timestampString
     }
     
+    var entry: Entry? { didSet { updateViews() }}
     
     @IBOutlet weak var entryTitleLabel: UILabel!
     @IBOutlet weak var entryTimestampLabel: UILabel!
--- Journal/View Controllers/EntryDetailsViewController.swift
+++ Journal/View Controllers/EntryDetailsViewController.swift
@@ -10,24 +10,46 @@ import UIKit
 
 class EntryDetailsViewController: UIViewController {
 
+    // MARK:- View lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
     private func updateViews() {
+        guard let entry = entry else {
+            
+            title = "New entry"
+            
+            return
+        }
         
+        title = entry.title
         
-        
+        entryTitleTextField.text = entry.title
+        entryContentTextView.text = entry.bodyText
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
+        guard let entryController = entryController,
+              let titleText = entryTitleTextField.text,
+            let bodyText = entryContentTextView.text else { return }
         
+        guard let entry = entry else {
+            entryController.createEntry(with: titleText, bodyText: bodyText)
+            navigationController?.popViewController(animated: true)
+            return
+        }
         
+        entryController.update(entry: entry, with: titleText, bodyText: bodyText)
+        navigationController?.popViewController(animated: true)
     }
-
     
+    // MARK:- Properties & types
+    var entry: Entry?
+    var entryController: EntryController?
     
-    
+    // MARK:- IBOutlets
     @IBOutlet weak var entryTitleTextField: UITextField!
     @IBOutlet weak var entryContentTextView: UITextView!
 
