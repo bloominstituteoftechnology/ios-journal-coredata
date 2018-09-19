72b52826cd65f643e5158ba5f587c1f77b939901
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-19 13:12:03 -0600

    Started Day 3 branch.

---
+++ Journal Core Data/Journal Core Data/Model/Entry+Encodable.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Encodable.swift
+//  Journal Core Data
+//
+//  Created by Dillon McElhinney on 9/19/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
