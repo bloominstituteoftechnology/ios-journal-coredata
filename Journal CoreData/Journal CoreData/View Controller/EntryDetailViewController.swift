8f76cd7da8b2fb48645f770fd44f2be821fc4d58
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-24 16:28:36 -0500

    EntryDetailViewController scene completed. Subclass, then outlets and action created

---
+++ Journal CoreData/Journal CoreData/View Controller/EntryDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EntryDetailViewController.swift
+//  Journal CoreData
+//
+//  Created by Ilgar Ilyasov on 9/24/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    // MARK: - Lifecycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
