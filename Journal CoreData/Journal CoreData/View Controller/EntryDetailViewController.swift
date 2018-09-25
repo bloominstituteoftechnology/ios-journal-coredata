a472caafa5898ca3b7f9faabba7f0829577ca1c6
iyinraphael <iyinraphael@gmail.com>
2018-09-24 18:12:59 -0700

    stroy board and view controller set up

---
+++ Journal CoreData/Journal CoreData/View Controller/EntryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryDetailViewController.swift
+//  Journal CoreData
+//
+//  Created by Iyin Raphael on 9/24/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+
+}
---
+++ Journal CoreData/Journal CoreData/View Controller/EntryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EntryTableViewCell.swift
+//  Journal CoreData
+//
+//  Created by Iyin Raphael on 9/24/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var bodyTextLabel: UILabel!
+}
