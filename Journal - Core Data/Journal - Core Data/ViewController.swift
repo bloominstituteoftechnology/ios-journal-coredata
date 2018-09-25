14b0294b28452971d187e033adc528a647d81843
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-25 08:32:47 -0700

    initial Commit

---
+++ Journal - Core Data/Journal - Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal - Core Data
+//
+//  Created by Welinkton on 9/25/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
