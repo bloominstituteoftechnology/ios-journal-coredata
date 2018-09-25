fa04dcb9dc630d328b600c0c990420df00940e87
iyinraphael <iyinraphael@gmail.com>
2018-09-24 18:45:32 -0700

    cell view controller set up

--- Journal CoreData/Journal CoreData/View Controller/EntryTableViewCell.swift
+++ Journal CoreData/Journal CoreData/View Controller/EntryTableViewCell.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
-
-
-
+    
+    func updateView(){
+        if let entry = entry {
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeStyle = .medium
+            dateFormatter.dateStyle = .medium
+            guard let date = entry.date else {return}
+            dateLabel.text = dateFormatter.string(from:date)
+            titleLabel.text = entry.title
+            bodyTextLabel.text = entry.bodyText
+        }
+    
+    }
+    
+    var entry: Entry?{
+        didSet{
+            updateView()
+        }
+    }
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-    
     @IBOutlet weak var bodyTextLabel: UILabel!
 }
