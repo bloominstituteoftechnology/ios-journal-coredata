6026a784222c59643620685a7f664e60082cbed6
yzhukovs <yzhukovs@bumnetworks.com>
2018-11-05 15:57:11 -0500

    storyboard set up

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Yvette Zhukovsky on 11/5/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Yvette Zhukovsky on 11/5/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    @IBOutlet weak var timeStamp: UILabel!
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
