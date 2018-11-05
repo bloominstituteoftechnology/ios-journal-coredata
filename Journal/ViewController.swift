2571fc7045eff3f3453636df4b083d74a1b1fd10
Lotanna <sugabelly@gmail.com>
2018-11-05 18:56:06 -0500

    initial commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Lotanna Igwe-Odunze on 11/5/18.
+//  Copyright © 2018 Lotanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
