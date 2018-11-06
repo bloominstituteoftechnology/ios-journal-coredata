761a5ed121ff636bfcddc2c43f31a3d0a59e3850
hendoware <hendoware@gmail.com>
2018-11-06 18:07:52 -0500

    finished day 2

--- Journal/Journal/Model/Entry+Convenience.swift
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -9,17 +9,25 @@
 import Foundation
 import CoreData
 
+enum Moods: String, CaseIterable {
+    case happy = "😃",
+    sad = "😢",
+    neutral = "😐"
+}
+
 extension Entry {
     convenience init(title: String,
                      bodyText: String? = nil,
                      timestamp: Date = Date(),
                      identifier: String = UUID().uuidString,
+                     mood: String,
                      context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
         self.timestamp = timestamp
         self.identifier = identifier
+        self.mood = mood
     }
     
 }
--- Journal/Journal/View Controller/EntryDetailViewController.swift
+++ Journal/Journal/View Controller/EntryDetailViewController.swift
@@ -6,14 +6,29 @@ class EntryDetailViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     @IBAction func saveButton(_ sender: Any) {
         guard let title = textField.text, !title.isEmpty else {return}
         guard let text = textView.text, !text.isEmpty else {return}
+        let priorityIndex = segmentedControl.selectedSegmentIndex
+        let priority = Moods.allCases[priorityIndex]
         
         if let entry = entry {
-            entryController?.updateEntry(entry: entry, title: title, bodyText: text)
+            entry.title = title
+            entry.bodyText = text
+            entry.mood = priority.rawValue
+            //entryController?.updateEntry(entry: entry, title: title, bodyText: text, mood: priority)
         } else {
-            entryController?.newEntry(title: title, bodyText: text)
+            //entryController?.newEntry(title: title, bodyText: text, mood: priority.rawValue)
+            _ = Entry(title: title, bodyText: text, mood: priority.rawValue)
+        }
+        
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            try moc.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
         }
         
         navigationController?.popViewController(animated: true)
@@ -37,5 +52,18 @@ class EntryDetailViewController: UIViewController {
         title = entry?.title ?? "Create New Entry"
         textField.text = entry?.title
         textView.text = entry?.bodyText
+        let mood = Moods.neutral.rawValue
+        
+        segmentedControl.selectedSegmentIndex = Moods.allCases.map({$0.rawValue}).index(of: mood)!
+        
+        
+//        let mood: Moods
+//        if let moodPriority = entry?.mood {
+//            mood = Moods(rawValue: moodPriority)!
+//        } else {
+//            print("why won't you be happy xcode")
+//            //mood = "😢"
+//        }
+        //segmentedControl.selectedSegmentIndex = Moods.allCases.index(of: mood)!
     }
 }
