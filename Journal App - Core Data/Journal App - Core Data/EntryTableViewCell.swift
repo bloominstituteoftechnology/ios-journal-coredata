74535b04bddf0ea6f84312ed0a0faa95989cbc58
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-21 16:54:39 -0600

    Table View Controller and custom cell built

---
+++ Journal App - Core Data/Journal App - Core Data/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleOutlet: UILabel!
+    
+    @IBOutlet weak var bodyOutlet: UILabel!
+    
+    @IBOutlet weak var timestampOutlet: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
