3034035f16d46db358fe0beeca9ee50390b56846
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-21 17:03:55 -0600

    Detail View Controller set up

---
+++ Journal App - Core Data/Journal App - Core Data/EntryDetailViewController.swift
@@ -0,0 +1,31 @@
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleOutlet: UITextField!
+    @IBOutlet weak var bodyOutlet: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
