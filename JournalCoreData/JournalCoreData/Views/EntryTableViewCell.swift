ab7b8773863c1b5638530f702d5e847da9c8c2ed
Nelson Gonzalez <nelglezfl@gmail.com>
2019-02-11 14:49:41 -0500

    Add date string extension. Implement cell

--- JournalCoreData/JournalCoreData/Views/EntryTableViewCell.swift
+++ JournalCoreData/JournalCoreData/Views/EntryTableViewCell.swift
@@ -14,12 +14,24 @@ class EntryTableViewCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    
+    private func updateViews(){
+        guard let entries = entry else {return}
+        titleLabel.text = entries.title
+        if let date = entries.timestamp {
+        dateLabel.text = .dateToString(date: date)
+        }
+        descriptionLabel.text = entries.bodyText
+    }
 
 }
