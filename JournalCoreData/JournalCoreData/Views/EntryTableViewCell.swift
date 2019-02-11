02849c1736048b463be396138e87effaa7d5023a
Angel <angelbuenrostro@outlook.com>
2019-02-11 16:42:16 -0500

    Add storyboards and custom controllers

---
+++ JournalCoreData/JournalCoreData/Views/EntryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EntryTableViewCell.swift
+//  JournalCoreData
+//
+//  Created by Angel Buenrostro on 2/11/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+}
