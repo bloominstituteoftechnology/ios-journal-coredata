0b1e8dbd63ccbfd1fe61d273dd03d5e5918465c2
Frulwinn <frulwinn@gmail.com>
2019-02-11 12:11:08 -0800

    initial commit

---
+++ journal/journal/View Controllers/DetailViewController.swift
@@ -0,0 +1,14 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
