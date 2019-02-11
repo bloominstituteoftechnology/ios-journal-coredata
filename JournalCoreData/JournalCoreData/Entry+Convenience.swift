203591539c72de8dc1d03fe032f9f1f4c44de03f
Angel <angelbuenrostro@outlook.com>
2019-02-11 17:48:35 -0500

    Add Entry with convenience init

---
+++ JournalCoreData/JournalCoreData/Entry+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Entry+Convenience.swift
+//  JournalCoreData
+//
+//  Created by Angel Buenrostro on 2/11/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import CoreData
+
+extension Entry {
+    
+    convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
