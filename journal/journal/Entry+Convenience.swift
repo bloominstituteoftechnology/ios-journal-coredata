6758ff984d627d27babac5b67c1a02164f9a09ec
Frulwinn <frulwinn@gmail.com>
2019-02-11 13:22:27 -0800

    added entry convenience and initialized managed object context

---
+++ journal/journal/Entry+Convenience.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import CoreData
+
+extension Entry {
+    
+    convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+        
+    }
+}
