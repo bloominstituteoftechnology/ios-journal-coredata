59ec60ced0cc6609f1aa012a38d3941aa0ea897b
Nelson Gonzalez <nelglezfl@gmail.com>
2019-02-11 11:27:52 -0500

    Add scenes with corresponding swift files

---
+++ JournalCoreData/JournalCoreData/EntriesTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EntriesTableViewController.swift
+//  JournalCoreData
+//
+//  Created by Nelson Gonzalez on 2/11/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath) as! EntryTableViewCell
+
+        
+
+        return cell
+    }
+   
+
+   
+   
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+   
+
+    // MARK: - Navigation
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+}
---
+++ JournalCoreData/JournalCoreData/EntryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EntryTableViewCell.swift
+//  JournalCoreData
+//
+//  Created by Nelson Gonzalez on 2/11/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
