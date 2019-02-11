d43f2284b77875b3cdd9ab920dce747e7636730a
Nelson Gonzalez <nelglezfl@gmail.com>
2019-02-11 11:32:44 -0500

    Add EntryDetailViewController scene and swift file with outlets/actions

---
+++ JournalCoreData/JournalCoreData/EntryDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JournalCoreData
+//
+//  Created by Nelson Gonzalez on 2/11/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func saveBarButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
