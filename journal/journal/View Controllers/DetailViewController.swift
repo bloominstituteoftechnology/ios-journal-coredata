cd2ef3bb10777a90261cb21c1861b346783f482e
Frulwinn <frulwinn@gmail.com>
2019-02-11 16:25:59 -0800

    add properties, logic to save button, and a updateViews func to detailviewcontroller

--- journal/journal/View Controllers/DetailViewController.swift
+++ journal/journal/View Controllers/DetailViewController.swift
@@ -5,21 +5,44 @@ import UIKit
 class DetailViewController: UIViewController {
     
     //MARK: - Properties
-    var entry: Entry? 
-    
+    var entryController: EntryController?
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var storyView: UITextView!
     @IBAction func save(_ sender: Any) {
+        guard let title = nameTextField.text, !title.isEmpty,
+        let bodyText = storyView.text else { return }
+        Entry(title: title, bodyText: bodyText)
         
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            try entryController?.saveToPersistentStore()
+        } catch {
+            NSLog ("error saving managed object context: \(error)")
+        }
+        navigationController?.popViewController(animated: true)
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateViews()
     }
-
-
+    
+    func updateViews() {
+        if isViewLoaded {
+            guard let entry = entry else {
+                title = "CreateEntry"
+                return
+            }
+            title = entry.title
+            nameTextField.text = entry.title
+            storyView.text = entry.bodyText
+        }
+    }
 }
 
