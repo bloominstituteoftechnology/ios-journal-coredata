f0b4c228d3b22303ca300c880863eab522980c88
Angel <angelbuenrostro@outlook.com>
2019-02-12 17:44:43 -0500

    Fix delete rows bug

--- JournalCoreData/JournalCoreData/Views/EntriesTableViewController.swift
+++ JournalCoreData/JournalCoreData/Views/EntriesTableViewController.swift
@@ -47,30 +47,18 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
             let entry = fetchedResultsController.object(at: indexPath)
             entryController.deleteEntry(entry: entry)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
 
     
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let destinationVC = segue.destination as! EntryDetailViewController
