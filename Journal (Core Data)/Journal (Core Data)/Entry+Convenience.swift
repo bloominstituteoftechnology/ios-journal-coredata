62606803b1e0dfa8106995352bd7354b34efa6f1
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-02-18 01:35:10 -0500

    create convenience init for Entry

---
+++ Journal (Core Data)/Journal (Core Data)/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal (Core Data)
+//
+//  Created by Julian A. Fordyce on 2/18/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
