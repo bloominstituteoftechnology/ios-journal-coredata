bfa38bb5703f4131d12b6a31d6db03c10eef8efe
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-02-18 00:59:14 -0500

    made subclass for tableview cell and connect outlets

---
+++ Journal (Core Data)/Journal (Core Data)/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal (Core Data)
+//
+//  Created by Julian A. Fordyce on 2/18/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
