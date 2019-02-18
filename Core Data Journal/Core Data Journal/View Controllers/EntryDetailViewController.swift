ade6a20df59df9f88d8ea171f0f07bc89d1786ef
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-18 14:29:47 -0800

    create the storyboards views by hand

---
+++ Core Data Journal/Core Data Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EntryDetailViewController.swift
+//  Core Data Journal
+//
+//  Created by Jaspal on 2/18/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
---
+++ Core Data Journal/Core Data Journal/View Controllers/JournalTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  JournalTableViewController.swift
+//  Core Data Journal
+//
+//  Created by Jaspal on 2/18/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
