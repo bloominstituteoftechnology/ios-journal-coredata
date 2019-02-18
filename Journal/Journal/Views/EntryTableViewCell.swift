d9ccb93b0e3da0a521dbad86a90a484393e896f4
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-18 13:35:50 -0800

    Add cell elements.

--- Journal/Journal/Views/EntryTableViewCell.swift
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
+    func updateViews() {
+        guard let entry = entry,
+        let timestamp = entry.timestamp else { return }
+        titleLabel.text = entry.name
+        detailsLabel.text = entry.bodyText
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy, HH:mm"
+        dateLabel.text = dateFormatter.string(from: timestamp)
+    }
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailsLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 }
