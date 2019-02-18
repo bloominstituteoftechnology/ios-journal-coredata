48c024c9f44b4def08bac1d642708433743de71d
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-18 13:42:31 -0800

    Add detailVC functionality.

--- Journal/Journal/View Controllers/JournalDetailViewController.swift
+++ Journal/Journal/View Controllers/JournalDetailViewController.swift
@@ -15,16 +15,35 @@ class JournalDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let entryController = entryController,
+        let title = titleTextField.text,
+        let body = detailsTextView.text else { return }
         
+        if let entry = entry {
+            entryController.update(name: title, body: body, entry: entry)
+        } else {
+            entryController.create(name: title, body: body)
+        }
+        navigationController?.popViewController(animated: true)
     }
     
     func updateViews() {
-
+        guard let entry = entry else {
+            navigationItem.title = "Create Entry"
+            return
+        }
+        navigationItem.title = entry.name
+        titleTextField.text = entry.name
+        detailsTextView.text = entry.bodyText
     }
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var detailsTextView: UITextView!
-    
-    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    var entryController: EntryController?
     
 }
