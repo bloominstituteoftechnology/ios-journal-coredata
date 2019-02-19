4f8169ab00eed23b2a258f58a0296f334751cafb
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-19 02:41:17 -0800

    complete detail view controller for entries

--- Core Data Journal/Core Data Journal/View Controllers/EntryDetailViewController.swift
+++ Core Data Journal/Core Data Journal/View Controllers/EntryDetailViewController.swift
@@ -13,29 +13,51 @@ class EntryDetailViewController: UIViewController {
     
     @IBAction func save(_ sender: Any) {
         
+        let title = titleTextField!.text
+
+        let bodyText = entryTextView!.text
+        
+        if let entry = entry {
+            entryController?.update(title: title!, bodyText: bodyText!, entry: entry)
+        } else {
+            entryController?.create(title: title!, bodyText: bodyText!)
+        }
+        
+        navigationController?.popViewController(animated: true)
+        
+        
     }
     
     @IBOutlet weak var titleTextField: UITextField!
     
-    @IBOutlet weak var entryTextField: UITextView!
+    @IBOutlet weak var entryTextView: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-    var entry: Entry?
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
     
     var entryController: EntryController?
 
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard isViewLoaded else { return }
+        
+        if entry?.title == nil {
+            navigationItem.title = "Create Entry"
+        } else {
+            navigationItem.title = entry?.title
+        }
+        
+        titleTextField.text = entry?.title
+        entryTextView.text = entry?.bodyText
+        
     }
 
 }
