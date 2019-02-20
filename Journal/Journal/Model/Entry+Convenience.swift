69a4a0627b85da964ed4f57599b63e0534017467
Paul Yi <yipaulc@gmail.com>
2019-02-19 17:49:09 -0800

    Update data model and initialize new mood property

--- Journal/Journal/Model/Entry+Convenience.swift
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -8,12 +8,23 @@
 
 import CoreData
 
+enum EntryMood: String {
+    case happy = "🙂"
+    case neutral = "😐"
+    case sad = "☹️"
+    
+    static var allMoods: [EntryMood] {
+        return  [.happy, .sad, .neutral]
+    }
+}
+
 extension Entry {
-    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .neutral, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
         self.timestamp = timestamp
         self.identifier = identifier
+        self.mood = mood.rawValue
     }
 }
