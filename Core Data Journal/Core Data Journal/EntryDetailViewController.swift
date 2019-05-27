1d0e2df819a0c6155d233f86348a9aa428104858
Michael Redig <mredig@gmail.com>
2019-05-27 17:57:02 -0500

    set up detail view controller

--- Core Data Journal/Core Data Journal/EntryDetailViewController.swift
+++ Core Data Journal/Core Data Journal/EntryDetailViewController.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 	var entryController: EntryController?
-	var entry: Entry?
+	var entry: Entry? {
+		didSet {
+			updateViews()
+		}
+	}
 
 	@IBOutlet var titleTextField: UITextField!
 	@IBOutlet var entryTextView: UITextView!
-	
+
+	override func viewDidLoad() {
+		updateViews()
+	}
+
+	private func updateViews() {
+		guard isViewLoaded else { return }
+		navigationItem.title = entry?.title ?? "Create New Entry"
+		titleTextField.text = entry?.title
+		entryTextView.text = entry?.bodyText
+	}
+
 	@IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+		guard let title = titleTextField.text, !title.isEmpty,
+			let bodyText = entryTextView.text, !bodyText.isEmpty else { return }
+
+		if let entry = entry {
+			entryController?.update(withTitle: title, andBody: bodyText, onEntry: entry)
+		} else {
+			entryController?.create(entryWithTitle: title, andBody: bodyText)
+		}
+
+		navigationController?.popViewController(animated: true)
 	}
 }
 
