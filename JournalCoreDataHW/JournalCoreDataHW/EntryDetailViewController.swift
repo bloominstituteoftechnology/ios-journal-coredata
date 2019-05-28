30a9722c3573380c038d9714e50e11ce3d0d8dc3
Michael R.F <mrflowers22@uwalumni.com>
2019-05-28 16:11:08 -0400

    created the detail view controller scene and its applicable file

---
+++ JournalCoreDataHW/JournalCoreDataHW/EntryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetailViewController.swift
+//  JournalCoreDataHW
+//
+//  Created by Michael Flowers on 5/28/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
