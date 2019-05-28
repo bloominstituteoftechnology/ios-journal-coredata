c21ef686693cd77179b18cc3d19025b56e141b42
Michael R.F <mrflowers22@uwalumni.com>
2019-05-28 16:07:25 -0400

    a

---
+++ JournalCoreDataHW/JournalCoreDataHW/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  JournalCoreDataHW
+//
+//  Created by Michael Flowers on 5/28/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var entryTitlelabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+
+}
