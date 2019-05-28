a0329bcd9bd4afce1e77f52483836bf07cf382b8
Michael R.F <mrflowers22@uwalumni.com>
2019-05-28 16:51:04 -0400

    implemented the logic for the table view controller

--- JournalCoreDataHW/JournalCoreDataHW/Views/Table View Controllers/Table View Cells/EntryTableViewCell.swift
+++ JournalCoreDataHW/JournalCoreDataHW/Views/Table View Controllers/Table View Cells/EntryTableViewCell.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var entryTitlelabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     
+    
+    private func updateViews(){
+        guard let passedInEntry = entry else { return }
+        entryTitlelabel.text = passedInEntry.title
+        timestampLabel.text = passedInEntry.timestamp?.description
+        bodyLabel.text = passedInEntry.bodyText
+    }
 
 }
