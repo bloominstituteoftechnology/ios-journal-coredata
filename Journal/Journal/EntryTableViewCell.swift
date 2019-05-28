86dc72d143bd380757031328b246f0e773b2785f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-28 12:57:04 -0700

    EntryTableViewCell

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Hector Steven on 5/28/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+   
+
+}
