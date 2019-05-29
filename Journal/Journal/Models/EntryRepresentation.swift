f25507000631c9f2bbf8203afce207cc7c25274a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-29 15:56:35 -0700

    EntryRepresentation start

---
+++ Journal/Journal/Models/EntryRepresentation.swift
@@ -0,0 +1,15 @@
+//
+//  EntryRepresentation.swift
+//  Journal
+//
+//  Created by Hector Steven on 5/29/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+struct EntryRepresentation: Codable {
+	
+	
+	
+}
