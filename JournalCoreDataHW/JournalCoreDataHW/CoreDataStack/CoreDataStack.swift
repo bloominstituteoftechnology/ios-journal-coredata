ccb532a5f1240cdeee2bddd2ab0e1af39a78e0e2
Michael R.F <mrflowers22@uwalumni.com>
2019-05-30 19:41:41 -0400

    implemented a generic function to save to the context (either main or background).

--- JournalCoreDataHW/JournalCoreDataHW/CoreDataStack/CoreDataStack.swift
+++ JournalCoreDataHW/JournalCoreDataHW/CoreDataStack/CoreDataStack.swift
@@ -19,11 +19,30 @@ class CoreDataStack {
                 print("Failed to laod the persistent store: \(error)")
             }
         }
+        //merges changes between two contexts
+        container.viewContext.automaticallyMergesChangesFromParent = true
         return container
     }()
     
     var mainContext: NSManagedObjectContext {
         return container.viewContext
+    }
+    //A generic function to save any context we want ( main or background ).
+    func save(context: NSManagedObjectContext) throws { //throws because of the do try catch we will add
+        var error: Error? //placeholder for the potential error. We will set its value with the error from the do try catch block
+        
+        //#1. Rule of core data: Don't use core data objects outside of a perform() block. i.e. performAndWait ( the equivalent to dispatchQueue.main.asynch )
+        do {
+            try context.save()
+        } catch let saveError {
+            print("Error saving to moc: \(saveError.localizedDescription)")
+            error = saveError
+        }
+        
+        // if there was an error, throw the function
+        if let error = error {
+            throw error
+        }
     }
 }
 
