b813d1068ab5166aff795180941f524db57af786
Sameera Leola <pixelandbyte@gmail.com>
2019-06-02 23:06:35 -0500

    Completed storyboard

---
+++ DayOne/Journal/View Controllers/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal - Day One
+//
+//  Created by Sameera Roussi on 6/2/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
