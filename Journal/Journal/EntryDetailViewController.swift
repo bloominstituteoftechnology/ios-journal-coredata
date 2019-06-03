0943baae570047bade841c9991680b4275520fc0
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-03 15:13:29 -0600

    Part 1 complete
    Main.storyboard setup configured

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Mitchell Budge on 6/3/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - Properties
+   
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+}
