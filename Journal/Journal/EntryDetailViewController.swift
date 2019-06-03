d98ac019d9bd50851157e6aca3540bfbae2e86d0
Jeremy Taylor <jtaylor@jctaylor.org>
2019-06-03 16:01:02 -0500

    Storyboard setup done

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Jeremy Taylor on 6/3/19.
+//  Copyright © 2019 Bytes Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Jeremy Taylor on 6/3/19.
+//  Copyright © 2019 Bytes Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var entryTextLabel: UILabel!
+    
+    
+}
