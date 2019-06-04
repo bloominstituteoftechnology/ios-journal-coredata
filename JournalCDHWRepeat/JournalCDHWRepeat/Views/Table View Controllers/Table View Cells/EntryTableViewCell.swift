fad54afdd0e28fbc56c698d88b79f75c447342e3
Michael R.F <mrflowers22@uwalumni.com>
2019-06-04 07:33:31 -0400

    implemented updateViews function in the table view cell

--- JournalCDHWRepeat/JournalCDHWRepeat/Views/Table View Controllers/Table View Cells/EntryTableViewCell.swift
+++ JournalCDHWRepeat/JournalCDHWRepeat/Views/Table View Controllers/Table View Cells/EntryTableViewCell.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     
- 
+    private func updateViews(){
+        guard let passedInEntry = entry else { return }
+        titleLabel.text = passedInEntry.title
+        bodyLabel.text = passedInEntry.bodyText
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        
+        if let date = passedInEntry.timestamp {
+            dateLabel.text = formatter.string(from: date)
+        }
+    }
+    
 }
