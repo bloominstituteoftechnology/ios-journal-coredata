f50fdda96bbe8f33d53b258841b7489a2160aa60
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-06-10 15:26:17 -0400

    Created EntryDetailViewController and setup textField and TextView.  Added Save Bar Button Item.

---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Thomas Cacciatore on 6/10/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+
+   
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var bodyTextView: UITextView!
+}
