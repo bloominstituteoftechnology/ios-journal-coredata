3326f697ab912e918ad1d370cbb896f27abf894b
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-06-10 16:51:02 -0400

    Updated EntryTableViewCell class file.

--- Journal/Journal/View Controllers/EntryTableViewCell.swift
+++ Journal/Journal/View Controllers/EntryTableViewCell.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    private func updateViews() {
+        guard let entry = entry else { return }
+        titleLabel.text = entry.title
+        bodyLabel.text = entry.bodyText
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyy-MM-dd HH:mm:ss"
+        let dateAsString = dateFormatter.string(from: entry.timeStamp!)
+        timeStampLabel.text = dateAsString
     }
-
+    
+    
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeStampLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
-    
 
 }
