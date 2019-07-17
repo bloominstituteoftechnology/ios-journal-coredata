7f9c8df47c09dd390d53bad3b7e83c8d880e2b40
Enayatullah Naseri <ena.nas@icloud.com>
2019-07-16 19:44:54 -0700

    Segmant controller works

--- Journal/Journal/Models/Entry+Convenience.swift
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -9,14 +9,26 @@
 import Foundation
 import CoreData
 
+enum EntryMood: String, CaseIterable {
+    
+    case 😕
+    case 😐
+    case 🙂
+    
+//    static var allMoods: [EntryMood] {
+//        return [.😕, .😐, .🙂]
+//    }
+}
+
 extension Entry {
     
-    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .😐, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
         self.timestamp = timestamp
         self.identifier = identifier
+        self.mood = mood.rawValue
     }
     
 }
--- Journal/Journal/View Controllers/EntryDetailViewController.swift
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -13,6 +13,7 @@ class EntryDetailViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var detailView: UITextView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var moodSegmentControl: UISegmentedControl!
     
     var entryController: EntryController?
     
@@ -32,18 +33,56 @@ class EntryDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
-        guard let titleEntry = titleTextField.text,
-            let detailEntry = detailView.text else {return}
-        if let entry = entries {
-            entryController?.update(entry: entry, title: titleEntry, bodyText: detailEntry)
+        
+        guard let entryName = self.titleTextField.text,
+            !entryName.isEmpty else {return}
+        
+        let moodIndex = moodSegmentControl.selectedSegmentIndex
+        let mood = EntryMood.allCases[moodIndex]
+        let bodyText = self.detailView.text
+        
+        if let entry = self.entries {
+            entry.title = entryName
+            entry.mood = mood.rawValue
+            entry.bodyText = bodyText
+            
         } else {
-            entryController?.createEntry(title: titleEntry, bodyText: detailEntry)
+            let _ = Entry(title: entryName, bodyText: bodyText!, mood: mood)
         }
+        
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            try moc.save()
+        } catch {
+            NSLog("Error Saving manged object context: \(error)")
+        }
+//        guard let titleEntry = titleTextField.text,
+//            let detailEntry = detailView.text else {return}
+//
+//        let moodIndex = moodSegmentControl.selectedSegmentIndex
+//        let mood = EntryMood.allCases[moodIndex]
+//
+//
+//        if let entry = entries {
+//            entryController?.update(entry: entry, title: titleEntry, bodyText: detailEntry, mood: mood)
+//            entry.mood = mood.rawValue
+//        } else {
+//            entryController?.createEntry(title: titleEntry, bodyText: detailEntry)
+//        }
         navigationController?.popViewController(animated: true)
     }
     
     func updateViews(){
         guard isViewLoaded else {return}
+        
+        let mood: EntryMood
+        if let entryMood = entries?.mood {
+            mood = EntryMood(rawValue: entryMood)!
+        } else {
+            mood = .😐
+        }
+        moodSegmentControl.selectedSegmentIndex = EntryMood.allCases.firstIndex(of: mood)!
+        
         self.navigationItem.title = entries?.title ?? "Entry"
         titleTextField.text = entries?.title
         detailView.text = entries?.bodyText
