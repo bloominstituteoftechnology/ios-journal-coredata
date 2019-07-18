afad4b282b20aaceac033b2b5b3823b7a52d198f
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-07-17 21:16:31 -0400

    Finished Part 2 - Syncing Databases
    - Completed Back to EntryController
    - Completed MVP of Day3

--- Journal/Journal/Model Controllers/EntryController.swift
+++ Journal/Journal/Model Controllers/EntryController.swift
@@ -14,6 +14,58 @@ let baseURL = URL(string: "https://journal-core-data-eed2a.firebaseio.com/")!
 class EntryController {
     typealias CompletionHandler = (Error?) -> Void
     
+    init() {
+        self.fetchEntrysFromServer()
+    }
+    
+    func fetchEntrysFromServer(completion: @escaping CompletionHandler = { _ in }) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching tasks: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else { NSLog("No data returned by the data task"); completion(error); return }
+            
+            DispatchQueue.main.async {
+                do {
+                    let entryReps = Array(try JSONDecoder().decode([String : EntryRepresentation].self, from: data).values)
+                    for entryRep in entryReps {
+                        let identifier = entryRep.identifier
+                        if let entry = self.fetchSingleEntryFromPersistentStore(forUUID: identifier) {
+                            self.update(entry: entry, with: entryRep)
+                        } else {
+                            let _ = Entry(entryRepresentation: entryRep)
+                        }
+                    }
+                    
+                    self.saveToPersistentStore()
+                    completion(nil)
+                } catch {
+                    NSLog("Error decoding task representations: \(error)")
+                    completion(error)
+                    return
+                }
+            }
+        }.resume()
+    }
+    
+    func fetchSingleEntryFromPersistentStore(forUUID uuid: String) -> Entry? {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", uuid)
+        
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            return try moc.fetch(fetchRequest).first
+        } catch {
+            NSLog("Error fetching entry with uuid \(uuid): \(error)")
+            return nil
+        }
+    }
+    
     func createEntry(withTitle title: String, withBodyText bodyText: String?, withMood mood: EntryMood) {
         let entry = Entry(title: title, bodyText: bodyText, mood: mood)
         self.saveToPersistentStore()
@@ -27,6 +79,14 @@ class EntryController {
         entry.timestamp = Date()
         self.saveToPersistentStore()
         self.put(entry: entry)
+    }
+    
+    // Update Entry with Entry Representation from Server
+    private func update(entry: Entry, with representation: EntryRepresentation) {
+        entry.title = representation.title
+        entry.bodyText = representation.bodyText
+        entry.mood = representation.mood
+        entry.timestamp = representation.timestamp
     }
     
     func deleteEntry(withEntry entry: Entry) {
