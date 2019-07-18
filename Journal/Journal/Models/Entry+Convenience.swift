35ab1a8a619ffff77c4fb6841805bcc49a4ec4d2
Enayatullah Naseri <ena.nas@icloud.com>
2019-07-18 15:25:13 -0700

    Updated Entry+Convenience.swift file and EntryRepresentation file

--- Journal/Journal/Models/Entry+Convenience.swift
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -22,7 +22,15 @@ enum EntryMood: String, CaseIterable {
 
 extension Entry {
     
-    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .🤪, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    var entryRepresentation: EntryRepresentation? {
+        guard let title = self.title,
+            let mood = self.mood,
+            let timestamp = self.timestamp,
+            let identifier = self.identifier else {return nil}
+        
+        return EntryRepresentation(title: title, bodyText: bodyText, mood: mood, timestamp: timestamp, identifier: identifier)
+    }
+    convenience init(title: String, bodyText: String?, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .🤪, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
@@ -31,4 +39,16 @@ extension Entry {
         self.mood = mood.rawValue
     }
     
+    // Initialize an entry object from EntryRepresentation
+    convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let mood = EntryMood(rawValue: entryRepresentation.mood!),
+            let identifier = UUID(uuidString: entryRepresentation.identifier) else {return nil }
+        
+        self.init(title: entryRepresentation.title,
+                  bodyText: entryRepresentation.bodyText,
+                  identifier: entryRepresentation.identifier,
+                  timestamp: entryRepresentation.timestamp,
+                  identifier: identifier.uuidString,
+                  mood: mood)
+    }
 }
