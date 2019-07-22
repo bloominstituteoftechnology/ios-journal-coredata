736e2fbb840de7726523b6bdd4c837d4ac53b8eb
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-22 15:48:16 -0400

    Set up entry controller and CRUD methods

---
+++ Journal/Journal/Controllers/EntryController.swift
@@ -0,0 +1,62 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Sean Acres on 7/22/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+    var entries: [Entry] {
+        return loadFromPersistentStore()
+    }
+    
+    func createEntry(title: String, bodyText: String?) {
+        Entry(title: title, bodyText: bodyText)
+        saveToPersistentStore()
+    }
+    
+    func updateEntry(entry: Entry, title: String, bodyText: String?) {
+        entry.title = title
+        entry.bodyText = bodyText
+        entry.timestamp = Date()
+        saveToPersistentStore()
+    }
+    
+    func deleteEntry(entry: Entry) {
+        let moc = CoreDataStack.shared.mainContext
+        
+        moc.delete(entry)
+        saveToPersistentStore()
+    }
+    
+    func saveToPersistentStore() {
+        let moc = CoreDataStack.shared.mainContext
+        
+        do {
+            try moc.save()
+        } catch {
+            NSLog("Error saving MOC: \(error)")
+            moc.reset()
+        }
+    }
+    
+    func loadFromPersistentStore() -> [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        
+        let moc = CoreDataStack.shared.mainContext
+        
+        do {
+            let entries = try moc.fetch(fetchRequest)
+            return entries
+        } catch {
+            NSLog("Error fetching tasks: \(error)")
+            return []
+        }
+    }
+    
+}
