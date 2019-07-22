b518a6308b4c03b74aa21d3714e8725d7a703dac
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-22 15:12:00 -0400

    Initial project setup

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Sean Acres on 7/22/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
