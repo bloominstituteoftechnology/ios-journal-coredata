42bd50c10f1de4e114bd72aea766aec5fcec85bd
Kat Milton <mltnkat@gmail.com>
2019-07-22 11:23:25 -0700

    Initial commit.

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Kat Milton on 7/22/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
