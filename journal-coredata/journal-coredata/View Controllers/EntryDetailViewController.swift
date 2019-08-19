9de8678a6f969b81a8e71da0664be1f975403be7
Alex Shillingford <alex.shillingford@icloud.com>
2019-08-19 14:24:32 -0600

    Complete Part 1: Soryboard layout

---
+++ journal-coredata/journal-coredata/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryDetailViewController.swift
+//  journal-coredata
+//
+//  Created by Alex Shillingford on 8/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+}
---
+++ journal-coredata/journal-coredata/Views/EntryTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EntryTableViewCell.swift
+//  journal-coredata
+//
+//  Created by Alex Shillingford on 8/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var entryNameLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
