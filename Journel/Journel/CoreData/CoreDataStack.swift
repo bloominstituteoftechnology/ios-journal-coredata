fb625eda7cd504dc1284bd41f4cd9369b435bc04
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-22 00:31:30 -0400

    Created CoreDataStack and Entry data model file. Entry+Convenience.

---
+++ Journel/Journel/CoreData/CoreDataStack.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataStack.swift
+//  Journel
+//
+//  Created by Nathan Hedgeman on 8/22/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Entry")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return self.container.viewContext
+    }
+}
