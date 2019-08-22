5b8f859d8760af0ff8223c8293ac7ec09af0c055
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-22 11:26:41 -0400

    Completed Fetch Result Controller delegate functions.

--- Journel/Journel/C/EntriesTableViewController.swift
+++ Journel/Journel/C/EntriesTableViewController.swift
@@ -113,24 +113,60 @@ class EntriesTableViewController: UITableViewController {
 
 extension EntriesTableViewController: NSFetchedResultsControllerDelegate {
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        <#code#>
+        tableView.beginUpdates()
     }
     
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        <#code#>
+        tableView.endUpdates()
     }
+    
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
                     didChange anObject: Any,
                     at indexPath: IndexPath?,
                     for type: NSFetchedResultsChangeType,
                     newIndexPath: IndexPath?) {
-        <#code#>
+        
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+            
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        case .move:
+            guard let newIndexPath = newIndexPath else { return }
+            guard let indexPath = indexPath else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+            
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .fade)
+            
+        @unknown default:
+            break
+        }
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
                     didChange sectionInfo: NSFetchedResultsSectionInfo,
                     atSectionIndex sectionIndex: Int,
                     for type: NSFetchedResultsChangeType) {
-        <#code#>
+        
+        let sections = IndexSet(integer: sectionIndex)
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(sections, with: .fade)
+            
+        case .delete:
+            tableView.deleteSections(sections, with: .fade)
+            
+        default:
+            break
+        }
     }
 }
+
