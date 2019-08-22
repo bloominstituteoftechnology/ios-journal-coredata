5f9c5f7e8ad879aff86bedb5a5092a2b2e910dcf
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-22 03:08:39 -0400

    Added entry variable	to Table View Cell and updateViews function.

--- Journel/Journel/C/EntryTableViewCell.swift
+++ Journel/Journel/C/EntryTableViewCell.swift
@@ -11,6 +11,11 @@ import UIKit
 class EntryTableViewCell: UITableViewCell {
     
     //Properties
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var textBodyLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
@@ -27,4 +32,29 @@ class EntryTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+//Functions
+extension EntryTableViewCell {
+    
+    func updateViews() {
+        titleLabel.text = entry?.title
+        textBodyLabel.text = String("\(entry?.bodyText?.maxLength(length: 15))...")
+    }
+}
+
+//Max character length
+extension String {
+    func maxLength(length: Int) -> String {
+        var string = self
+        let nsString = string as NSString
+        if nsString.length >= length {
+            string = nsString.substring(with:
+                NSRange(
+                    location: 0,
+                    length: nsString.length > length ? length : nsString.length)
+            )
+        }
+        return  string
+    }
 }
