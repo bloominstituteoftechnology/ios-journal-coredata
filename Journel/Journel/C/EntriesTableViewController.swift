150a66295931531935572c6fbdbbb3edc1adda1c
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-22 08:52:34 -0400

    Initial Day 2

---
+++ Journel/Journel/C/EntriesTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  EntriesTableViewController.swift
+//  Journel
+//
+//  Created by Nathan Hedgeman on 8/21/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+    
+    //Properties
+    let entryController = EntryController()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    // MARK: - Table view data source
+    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return entryController.entries.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath) as! EntryTableViewCell
+        let entry = entryController.entries[indexPath.row]
+        cell.entry = entry
+        cell.updateViews()
+        return cell
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let entry = entryController.entries[indexPath.row]
+            entryController.delete(entry: entry)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }    
+    }
+    
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CellDetailSegue" {
+            let detailVC = segue.destination as! EntryDetailViewController
+            guard let selectedEntryPath = tableView.indexPathForSelectedRow?.row else {return print("Selected row couldnt be found")}
+            let entry = entryController.entries[selectedEntryPath]
+            detailVC.entryController = entryController
+            detailVC.entry = entry
+        } else {
+            let detailVC = segue.destination as! EntryDetailViewController
+            detailVC.entryController = entryController
+        }
+    }
+    
+    
+}
---
+++ Journel/Journel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journel
+//
+//  Created by Nathan Hedgeman on 8/21/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
