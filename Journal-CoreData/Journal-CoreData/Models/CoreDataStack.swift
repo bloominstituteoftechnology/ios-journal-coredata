5557c31ace1062312c6f8b2402ae24cefbc27fe1
ladybeitel <ciarabeitel@gmail.com>
2019-09-16 19:17:45 -0400

    Part 2 - Entry and EntryController Setup Create CoreDataStack. Create Entry.

---
+++ Journal-CoreData/Journal-CoreData/Models/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  Journal-CoreData
+//
+//  Created by Ciara Beitel on 9/16/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    private init() {
+        
+    }
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Journal-CoreData")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        })
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func saveToPersistentStore() {
+        do {
+            try mainContext.save()
+        } catch {
+            NSLog("Error saving context: \(error)")
+            mainContext.reset()
+        }
+    }
+}
