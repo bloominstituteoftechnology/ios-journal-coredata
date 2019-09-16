a7920635b9d817ebcf6532a1c73b73bb57aa9e3c
Andrew Ruiz <nukedoom@gmail.com>
2019-09-16 14:04:30 -0700

    Initial Commit

---
+++ Journal CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal CoreData
+//
+//  Created by Andrew Ruiz on 9/16/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
