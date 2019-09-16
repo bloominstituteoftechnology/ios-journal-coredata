737383159e140a97cbc9e555d5ef082e989468c9
Alex Shillingford <alex.shillingford@icloud.com>
2019-09-16 14:23:24 -0600

    First commit

---
+++ Journal (Core Data)/Journal (Core Data)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal (Core Data)
+//
+//  Created by Alex Shillingford on 9/16/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
