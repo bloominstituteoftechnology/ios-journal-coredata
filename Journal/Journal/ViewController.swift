7368583202057cd644c57a7ccca93d335d81ff93
Jordan Christensen <jordan.c4922@gmail.com>
2019-09-17 03:22:10 +0800

    Create new xcode project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Jordan Christensen on 9/17/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
