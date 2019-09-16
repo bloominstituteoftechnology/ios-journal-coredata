992cab76b51ea2a2b62648b3118769c946211f18
AustinPotts <pottsevolvements@gmail.com>
2019-09-16 15:26:31 -0400

    Configured Task+Convenience File
    Using the Task extension to call a convenience init.

---
+++ CoreDataJournal/Model/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  CoreDataJournal
+//
+//  Created by Austin Potts on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ CoreDataJournal/Model/Task+Conveneince.swift
@@ -0,0 +1,31 @@
+//
+//  Task+Conveneince.swift
+//  CoreDataJournal
+//
+//  Created by Austin Potts on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+    
+    //Core Data creates the Task Class then we add function to it
+    convenience init(title: String, journalNote: String?, context: NSManagedObjectContext) {
+        
+        
+        //Setting up the generic NSManageObject functionality of the model object
+        self.init(context: context)
+        
+        //Once we have the object we can sculpt it
+        self.title = title
+        self.journalNote = journalNote
+        
+        
+    }
+    
+    
+    
+}
