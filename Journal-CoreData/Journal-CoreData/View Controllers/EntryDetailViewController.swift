02d8777ba898f1808d89f28b5f299e8335ccf9a5
ladybeitel <ciarabeitel@gmail.com>
2019-09-16 21:41:04 -0400

    Part 3 - View and View Controller Implementation

--- Journal-CoreData/Journal-CoreData/View Controllers/EntryDetailViewController.swift
+++ Journal-CoreData/Journal-CoreData/View Controllers/EntryDetailViewController.swift
@@ -10,19 +10,44 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
     
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    var entryController: EntryController?
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var storyTextView: UITextView!
     
     @IBAction func saveTapped(_ sender: Any) {
+        guard let titleTextField = titleTextField.text, let storyTextView = storyTextView.text else { return }
+        
+        if let entry = entry {
+            entryController?.updateEntry(entry: entry, title: titleTextField, bodyText: storyTextView)
+        } else {
+            entryController?.createEntry(title: titleTextField, bodyText: storyTextView, timestamp: Date(), identifier: titleTextField)
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
         // Do any additional setup after loading the view.
     }
     
+    func updateViews() {
+        if isViewLoaded == true {
+            title = entry?.title ?? "Create Entry"
+            titleTextField.text = entry?.title
+            storyTextView.text = entry?.bodyText
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
