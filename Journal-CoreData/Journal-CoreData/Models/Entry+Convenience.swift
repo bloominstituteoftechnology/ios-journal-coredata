2f46054eadb031fc11b91b62c1c78556b85a43ca
ladybeitel <ciarabeitel@gmail.com>
2019-09-16 21:41:04 -0400

    Part 3 - View and View Controller Implementation

---
+++ Journal-CoreData/Journal-CoreData/Models/Entry+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Entry+Convenience.swift
+//  Journal-CoreData
+//
+//  Created by Ciara Beitel on 9/16/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext) {
+    
+        self.init(context: context)
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
