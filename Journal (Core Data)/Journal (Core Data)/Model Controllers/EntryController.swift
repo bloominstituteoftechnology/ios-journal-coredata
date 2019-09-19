f061e1d2fe39fa78918cb6d5b1b8bb01a8aedb8d
Alex Shillingford <alex.shillingford@icloud.com>
2019-09-19 16:39:47 -0600

    Complete Day 3 Part 2

--- Journal (Core Data)/Journal (Core Data)/Model Controllers/EntryController.swift
+++ Journal (Core Data)/Journal (Core Data)/Model Controllers/EntryController.swift
@@ -9,6 +9,8 @@
 import Foundation
 import CoreData
 
+// MARK: - Enums
+
 enum HTTPMethod: String {
     case get = "GET"
     case put = "PUT"
@@ -16,21 +18,88 @@ enum HTTPMethod: String {
     case delete = "DELETE"
 }
 
-enum NetworkError: Error {
-    case noAuth
-    case badAuth
-    case otherError
-    case badData
-    case noDecode
-}
-
 class EntryController {
+    
+    // MARK: - Initializers
+    init() {
+        fetchEntriesFromServer()
+    }
+    
+    // MARK: - Properties
     
     var baseURL = URL(string: "https://ios-9-journal-core-data.firebaseio.com/")!
     
-//    var entries: [Entry] {
-//        return loadFromPersistentStore()
-//    }
+    //    var entries: [Entry] {
+    //        return loadFromPersistentStore()
+    //    }
+    
+    // MARK: - Methods and Functions
+    
+    func fetchEntriesFromServer(completion: @escaping (Error?) -> Void = { _ in }) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error GETting entries from Firebase: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            var entryRepresentations: [EntryRepresentation] = []
+            
+            do {
+                let reps = try JSONDecoder().decode([String: EntryRepresentation].self, from: data)
+                entryRepresentations = reps.compactMap({ $0.value })
+                self.updateEntries(with: entryRepresentations)
+                completion(nil)
+            } catch {
+                NSLog("Error Decoding entry representations on line \(#line) in \(#file): \(error)")
+                completion(error)
+                return
+            }
+            
+        }.resume()
+    }
+    
+    func update(entry: Entry, entryRepresentation: EntryRepresentation) {
+        entry.title = entryRepresentation.title
+        entry.bodyText = entryRepresentation.bodyText
+        entry.timeStamp = entryRepresentation.timeStamp
+        entry.mood = entryRepresentation.mood
+        entry.identifier = entryRepresentation.identifier
+    }
+    
+    func updateEntries(with representations: [EntryRepresentation]) {
+        let identifiersToFetch = representations.compactMap({ $0.identifier })
+        
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        let context = CoreDataStack.shared.mainContext
+        var entriesToCreate = representationsByID
+        
+        do {
+            let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+            let existingEntries = try context.fetch(fetchRequest)
+            
+            for entry in existingEntries {
+                guard let identifier = entry.identifier,
+                    let representation = representationsByID[identifier] else { return }
+                update(entry: entry, entryRepresentation: representation)
+                entriesToCreate.removeValue(forKey: identifier)
+            }
+            
+            for representation in entriesToCreate.values {
+                Entry(entryRepresentation: representation, context: context)
+            }
+            
+            saveToPersistentStore()
+        } catch {
+            NSLog("Error performing NSFetchRequest on line \(#line) in \(#file): \(error)")
+        }
+    }
     
     func saveToPersistentStore() {
         do {
@@ -70,7 +139,7 @@ class EntryController {
             }
             
             completion(nil)
-        }.resume()
+            }.resume()
     }
     
     func deleteEntryFromServer(entry: Entry, completion: @escaping (Error?) -> Void = { _ in }) {
@@ -86,20 +155,20 @@ class EntryController {
                 return
             }
             completion(nil)
-        }.resume()
+            }.resume()
     }
     
-//    func loadFromPersistentStore() -> [Entry] {
-//        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-//        
-//        do {
-//            let entries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
-//            return entries
-//        } catch {
-//            NSLog("Error fetching entries: \(error)")
-//            return []
-//        }
-//    }
+    //    func loadFromPersistentStore() -> [Entry] {
+    //        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+    //
+    //        do {
+    //            let entries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+    //            return entries
+    //        } catch {
+    //            NSLog("Error fetching entries: \(error)")
+    //            return []
+    //        }
+    //    }
     
     func createEntry(title: String, bodyText: String, timeStamp: Date, mood: Mood) {
         let entry = Entry(title: title, bodyText: bodyText, timeStamp: timeStamp, mood: mood, context: CoreDataStack.shared.mainContext)
