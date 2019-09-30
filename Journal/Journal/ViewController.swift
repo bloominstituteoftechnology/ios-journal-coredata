e3695129d81b720f700fbaa7f0491ab21e378ffa
John Kouris <jdkouris@gmail.com>
2019-09-30 14:50:35 -0700

    Created the skeleton Xcode project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by John Kouris on 9/30/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
