20c2d4d55a5bae81a68569f6562729508e8d1dff
John Kouris <jdkouris@gmail.com>
2019-10-01 08:45:20 -0700

    Created the skeleton EntryController class

---
+++ Journal/Journal/Model Controllers/EntryController.swift
@@ -0,0 +1,16 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by John Kouris on 10/1/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+    
+    
+}
