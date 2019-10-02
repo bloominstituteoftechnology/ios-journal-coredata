ee51dc372de3ea43c6b473b1152f80e55245fe90
Eoin Lavery <dev@tenantc.co.uk>
2019-10-02 18:58:28 +0100

    Added EntriesTableViewController and EntryTableViewCell

---
+++ MyJournal/MyJournal/CustomViews/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  MyJournal
+//
+//  Created by Eoin Lavery on 02/10/2019.
+//  Copyright © 2019 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    //MARK: - IBOUTLETS
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var entryTimestampLabel: UILabel!
+    @IBOutlet weak var entryNotesLabel: UILabel!
+
+}
