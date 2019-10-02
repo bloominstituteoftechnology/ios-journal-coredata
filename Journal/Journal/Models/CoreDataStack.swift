7515e2c6bb6c67620137fd80fddf0645f72a57f6
Vici Shaweddy <vshaweddy@gmail.com>
2019-10-02 11:14:17 -0400

    creating files

---
+++ Journal/Journal/Models/CoreDataStack.swift
@@ -0,0 +1,10 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Vici Shaweddy on 10/2/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+import CoreData
