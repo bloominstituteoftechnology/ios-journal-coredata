b31982976f272789fbf43be12d9af12cad9693df
Jessie Griffin <i.jessie.me@icloud.com>
2019-10-01 23:48:55 -0400

    Part 1 - Complete TableViewController and DetailViewController UI setup with outlets

---
+++ Journal/Journal/Custom Views/EntryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Jessie Ann Griffin on 10/1/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    
+    
+    
+}
---
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Jessie Ann Griffin on 10/1/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    
+}
