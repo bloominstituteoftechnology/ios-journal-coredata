fa2cb98ab30f04a2a40f9ef0eaf22bcb1afa7e48
Robert Keffury <bobby.keffury@me.com>
2019-10-02 10:58:21 -0700

    Finished TableViewController and TableViewCell implementation.

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Bobby Keffury on 10/2/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
