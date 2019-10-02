acff5f458d37f4203f232273ce2fb0ae047efbaf
Eoin Lavery <dev@tenantc.co.uk>
2019-10-02 18:35:43 +0100

    Initial Commit

---
+++ MyJournal/MyJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyJournal
+//
+//  Created by Eoin Lavery on 02/10/2019.
+//  Copyright © 2019 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
