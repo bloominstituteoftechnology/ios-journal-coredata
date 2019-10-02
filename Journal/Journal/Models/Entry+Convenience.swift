0aeaf57d0c931226e642ec1cb44097d3054d7a7e
Robert Keffury <bobby.keffury@me.com>
2019-10-02 11:34:49 -0700

    Added Entry Model and Extension

---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Bobby Keffury on 10/2/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String? = nil, timestamp: Date? = nil, identifier: String? = nil, bodyText: String? = nil, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.timestamp = timestamp
+        self.identifier = identifier
+        self.bodyText = bodyText
+    }
+    
+}
