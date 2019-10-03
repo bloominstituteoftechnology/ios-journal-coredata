c3a7cfabd14c5df614a54342afa269623de81562
dillonp23 <dillonp.lambda@gmail.com>
2019-10-02 19:55:12 -0700

    Initial commit. Add Xcode starter project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Dillon P on 10/2/19.
+//  Copyright © 2019 Lambda iOSPT2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
