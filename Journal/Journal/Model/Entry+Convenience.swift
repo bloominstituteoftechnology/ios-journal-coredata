142b9e933b4e2a119dfc5fadfef4d437cb262d15
Jessie Griffin <i.jessie.me@icloud.com>
2019-10-02 21:08:46 -0400

    Entry+Convenience setup complete

--- Journal/Journal/Model/Entry+Convenience.swift
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -10,5 +10,14 @@ import Foundation
 import CoreData
 
 extension Entry {
-    
+    convenience init(
+        title: String, bodyText: String?, timeStamp: Date, identifier: String,
+        context: NSManagedObjectContext = CoreDataStack.shared.mainContext)
+    {
+        self.init(context: context)
+        self.title = title
+        self.bodyText = bodyText
+        self.timeStamp = timeStamp
+        self.identifier = identifier
+    }
 }
