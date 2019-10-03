1a4dbe44a93254b2d7ef15a6cf73adf24616a365
Joel Groomer <joelgroomer@gmail.com>
2019-10-02 19:54:37 -0700

    Part 2 (EntryController) complete. CRUD methods implemented.

--- Journal/Journal/Controllers/EntryController.swift
+++ Journal/Journal/Controllers/EntryController.swift
@@ -7,30 +7,55 @@
 //
 
 import Foundation
+import CoreData
 
 class EntryController {
+    let moc = CoreDataStack.shared.mainContext
     
     var entries: [JournalEntry] {
         return self.loadFromPersistentStore()
     }
     
     func saveToPersistentStore() {
-        
+        do {
+            try moc.save()
+        } catch {
+            print("Error saving: \(error)")
+        }
     }
     
     func loadFromPersistentStore() -> [JournalEntry] {
+        let fetchRequest: NSFetchRequest<JournalEntry> = JournalEntry.fetchRequest()
         
+        do {
+            return try moc.fetch(fetchRequest)
+        } catch {
+            print("Error fetching: \(error)")
+            return []
+        }
     }
     
     func createEntry(title: String, body: String) {
-        
+        let _ = JournalEntry(title: title, bodyText: body)
+        saveToPersistentStore()
     }
     
     func updateEntry(entry: JournalEntry, newTitle: String, newBody: String) {
-        
+        guard !newTitle.isEmpty else { return }
+        entry.title = newTitle
+        entry.bodyText = newBody
+        entry.timestamp = Date()
+        saveToPersistentStore()
     }
     
     func deleteEntry(entry: JournalEntry) {
+        moc.delete(entry)
         
+        do {
+            try moc.save()
+        } catch {
+            moc.reset()
+            print("Error deleting entry: \(error)")
+        }
     }
 }
