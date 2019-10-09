34b0e32d8f0fb07e12d8260ce2f0a099be89040f
dillonp23 <dillonp.lambda@gmail.com>
2019-10-08 18:45:08 -0700

    Add EntryRepresentation.swift and struct

---
+++ Journal/Journal/Model/EntryRepresentation.swift
@@ -0,0 +1,20 @@
+//
+//  EntryRepresentation.swift
+//  Journal
+//
+//  Created by Dillon P on 10/8/19.
+//  Copyright © 2019 Lambda iOSPT2. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct EntryRepresentation {
+    
+    var title: String
+    var bodyText: String
+    var timestamp: Date
+    var mood: String
+    var identifier: String
+    
+}
