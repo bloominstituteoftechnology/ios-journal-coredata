6522f9ca0247610b8fe9eea95d08623dc611709e
Wilma <kima_92@fastmail.com>
2019-10-14 18:00:14 -0400

    Gave default values to Convenience Initializer

--- Journal/Models/Entry+Convenience.swift
+++ Journal/Models/Entry+Convenience.swift
@@ -11,7 +11,7 @@ import CoreData
 
 extension Entry {
     
-    @discardableResult convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext) {
+    @discardableResult convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = "", context: NSManagedObjectContext) {
         self.init(context: context)
         
         self.title = title
