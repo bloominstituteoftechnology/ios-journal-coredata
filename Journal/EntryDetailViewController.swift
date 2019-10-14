c3aa5d0df617e13cb0dd6090a449af9470513c35
Andrew Ruiz <nukedoom@gmail.com>
2019-10-14 15:30:35 -0700

    Completed storyboard.

---
+++ Journal/EntryDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Andrew Ruiz on 10/14/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var journalEntryTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+}
