068818396a3cbd9772b937d991a86609bba20ca4
Percy <percival.ngan@gmail.com>
2019-10-14 15:05:40 -0500

    Added TableView, Detailview, Segues

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Percy Ngan on 10/14/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
