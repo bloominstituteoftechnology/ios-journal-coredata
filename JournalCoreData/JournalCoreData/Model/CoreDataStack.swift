05ccafdbd9ab393d7dc0602c175942e175ab93a4
Jerry Haaser <jerry.haaser@hotmail.com>
2019-10-15 17:41:58 -0500

    set up models

---
+++ JournalCoreData/JournalCoreData/Model/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  JournalCoreData
+//
+//  Created by admin on 10/15/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    // Let us access the CoreDataStack from anywhere in the app.
+    static let shared = CoreDataStack()
+    
+    // Set up a persistent container
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Entry")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // Create easy access to the moc (managed object context)
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func saveToPersistentStore() {
+        do {
+            try mainContext.save()
+        } catch {
+            NSLog("Error saving context: \(error)")
+            mainContext.reset()
+        }
+    }
+}
