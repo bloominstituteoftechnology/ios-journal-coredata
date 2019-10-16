6353096c889c2c1d791c39f7261ed0b9eab04f2f
Isvvc <lyons@tuta.io>
2019-10-16 15:52:12 -0600

    Sync databases on app launch

--- Journal/Journal/Model Controllers/EntryController.swift
+++ Journal/Journal/Model Controllers/EntryController.swift
@@ -17,6 +17,10 @@ enum HTTPMethod: String {
 
 class EntryController {
     
+    init() {
+        fetchEntriesFromServer()
+    }
+    
     let baseURL = URL(string: "https://journal-5d828.firebaseio.com/")!
     
     func put(entry: Entry, completion: @escaping () -> Error? = { () -> Error? in return nil} ) {
@@ -74,11 +78,79 @@ class EntryController {
         
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             if let error = error {
-                NSLog("Error Deleting entry: \(error)")
+                NSLog("Error deleting entry: \(error)")
                 let _ = completion()
                 return
             }
         }.resume()
+    }
+    
+    func fetchEntriesFromServer(completion: @escaping  () -> Error? = { () -> Error? in return nil}) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error getting entries: \(error)")
+                let _ = completion()
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task.")
+                let _ = completion()
+                return
+            }
+            
+            var entryRepresentations: [EntryRepresentation] = []
+            
+            do {
+                entryRepresentations = try JSONDecoder().decode([String: EntryRepresentation].self, from: data).map({ $0.value })
+                self.updateEntries(with: entryRepresentations)
+            } catch {
+                NSLog("Error decoding entry representations: \(error)")
+            }
+            
+            let _ = completion()
+        }.resume()
+    }
+    
+    func updateEntries(with representations: [EntryRepresentation]) {
+        let context = CoreDataStack.shared.mainContext
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        
+        let identifiersToFetch = representations.map({ $0.identifier })
+        var representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+        
+        do {
+            let existingEntries = try context.fetch(fetchRequest)
+            
+            for entry in existingEntries {
+                guard let identifier = entry.identifier,
+                    let representation = representationsByID[identifier] else { continue }
+                
+                update(entry: entry, representation: representation)
+                
+                representationsByID.removeValue(forKey: identifier)
+            }
+            
+            for representation in representationsByID.values {
+                Entry(entryRepresentation: representation, context: context)
+            }
+            
+            saveToPersistentStore()
+        } catch {
+            NSLog("Error fetching existing entries: \(error)")
+        }
+    }
+    
+    func update(entry: Entry, representation entryRepresentation: EntryRepresentation) {
+        entry.title = entryRepresentation.title
+        entry.bodyText = entryRepresentation.bodyText
+        entry.timestamp = entryRepresentation.timestamp
+        entry.mood = entryRepresentation.mood
     }
     
     func saveToPersistentStore() {
