82bc7aca52e699cd1b8709f7c363e9d751ef1cca
Jon Bash <jonbash@protonmail.com>
2019-11-11 13:44:02 -0800

    mess w/ ui too much

--- Journal/Journal/EntryDetailViewController.swift
+++ Journal/Journal/EntryDetailViewController.swift
@@ -10,21 +10,9 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var entryTextView: UITextView!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
     }
-    */
-
 }
--- Journal/Journal/EntryTableViewCell.swift
+++ Journal/Journal/EntryTableViewCell.swift
@@ -9,16 +9,11 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var entryTextLabel: UILabel!
+    
 }
