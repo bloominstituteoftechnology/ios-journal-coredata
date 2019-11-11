02236fc3811354d6f49fe12b80d02947dac10afa
Rwoltx <richardx1016@gmail.com>
2019-11-11 18:08:30 -0500

    Storyboard Layout actually finished

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Rick Wolter on 11/11/19.
+//  Copyright © 2019 Richar Wolter. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
