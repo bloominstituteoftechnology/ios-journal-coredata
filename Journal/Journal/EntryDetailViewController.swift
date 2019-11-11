acf080fbb5f09b0bfe09f6fafae7e3822618813f
Jon Bash <jonbash@protonmail.com>
2019-11-11 13:09:51 -0800

    set up table ui

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Jon Bash on 2019-11-11.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Jon Bash on 2019-11-11.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
