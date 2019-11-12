baa7ebd28746175150978b6c10e9b67042901b60
Jon Bash <jonbash@protonmail.com>
2019-11-11 16:47:03 -0800

    cell constraints working at last

--- Journal/Journal/Views/EntryTableViewCell.swift
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -14,16 +14,15 @@ class EntryTableViewCell: UITableViewCell {
     
     var entry: Entry? {
         didSet {
-            setUpSubViews()
             updateViews()
         }
     }
     
     // MARK: - UI Elements
     
-    var titleLabel: UILabel!
-    var timestampLabel: UILabel!
-    var bodyLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
     
     // MARK: - Methods
     
@@ -36,44 +35,5 @@ class EntryTableViewCell: UITableViewCell {
         titleLabel.text = entry.title ?? ""
         timestampLabel.text = "\(entry.timestamp ?? Date())"
         bodyLabel.text = entry.bodyText ?? ""
-    }
-    
-    func setUpSubViews() {
-        titleLabel = UILabel()
-        addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        let titleHorizontalResistance = titleLabel.contentHuggingPriority(for: .horizontal) - 1
-        titleLabel.setContentHuggingPriority(titleHorizontalResistance, for: .horizontal)
-        
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
-            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8)
-        ])
-        
-        timestampLabel = UILabel()
-        timestampLabel.textAlignment = .right
-        addSubview(timestampLabel)
-        timestampLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let timestampLeadingAnchor = timestampLabel.leadingAnchor.constraint(greaterThanOrEqualTo: titleLabel.trailingAnchor, constant: 4)
-        timestampLeadingAnchor.priority -= 1
-        NSLayoutConstraint.activate([
-            timestampLeadingAnchor,
-            timestampLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
-            timestampLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-        ])
-        
-        bodyLabel = UILabel()
-        bodyLabel.numberOfLines = 3
-        addSubview(bodyLabel)
-        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            bodyLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
-            bodyLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
-            bodyLabel.topAnchor.constraint(greaterThanOrEqualTo: titleLabel.bottomAnchor, constant: 8),
-            bodyLabel.topAnchor.constraint(greaterThanOrEqualTo: timestampLabel.bottomAnchor, constant: 8),
-            bodyLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 12),
-            bodyLabel.heightAnchor.constraint(equalToConstant: 60)
-        ])
     }
 }
