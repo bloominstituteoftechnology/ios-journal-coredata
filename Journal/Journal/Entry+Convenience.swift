34b58661e5a34dd7544d7682a8b5274d4088331f
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-12 12:36:41 -0800

    day 2 inital com

---
+++ Journal/Journal/Entry+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Lambda_School_Loaner_204 on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String, timestamp: Date, bodyText: String? = nil, identifier: String? = nil, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.timestamp = timestamp
+        self.bodyText = bodyText
+        self.identifier = identifier
+    }
+}
