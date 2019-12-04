37f4686b50f464cbc726e700e5856156259d5bcd
Craig Swanson <cswanson02@me.com>
2019-12-03 23:29:27 -0600

    Created storyboard and UI elements.

---
+++ Journal/Journal/JournalDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  JournalDetailViewController.swift
+//  Journal
+//
+//  Created by Craig Swanson on 12/3/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
