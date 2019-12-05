29a8d5a6b191da815caca0c990b7d32b00e367fe
Joseph Rogers <joerogersss2016@gmail.com>
2019-12-04 20:35:00 -0600

    storyboard layout complete

---
+++ Journal/Journal/Controllers/JournalTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  JournalTableViewCell.swift
+//  Journal
+//
+//  Created by Joseph Rogers on 12/4/19.
+//  Copyright © 2019 Moka Apps. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
