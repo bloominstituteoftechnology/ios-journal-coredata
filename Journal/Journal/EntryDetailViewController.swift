068db210510cac93a42d708e076efac521d3deab
Denis Cedeno <denis.cedeno@gmail.com>
2019-12-04 21:30:39 -0500

    completed part 1, added view controller and detail view controller. Wired outlets and save action

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by denis cedeno on 12/4/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    @IBOutlet weak var entryTextField: UITextField!
+    
+    @IBOutlet weak var entryTextView: UITextView!
+    @IBAction func saveEntry(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
