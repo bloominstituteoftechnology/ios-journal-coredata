38e6e9d10311050ba286ad00baa04e9b161330e4
Jessie <jessie.griffin@icloud.com>
2019-12-04 21:55:51 -0500

    Adds CoreDataStack and Entry data model

---
+++ JournalPT3/JournalPT3/Entry+Convenience.swift
@@ -0,0 +1,24 @@
+//
+//  Entry+Convenience.swift
+//  JournalPT3
+//
+//  Created by Jessie Ann Griffin on 12/4/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String,
+                     bodyText: String? = nil,
+                     timestamp: Date,
+                     identifier: String,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
