94d61d8c49f1686d2ac29f86a04514bf9de74ae1
Jessie <jessie.griffin@icloud.com>
2019-12-04 21:41:50 -0500

    Part 1 Finished Storyboard and ViewController Files

---
+++ JournalPT3/JournalPT3/EntryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryViewController.swift
+//  JournalPT3
+//
+//  Created by Jessie Ann Griffin on 12/4/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodytextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func saveEntry(_ sender: Any) {
+    }
+    
+}
