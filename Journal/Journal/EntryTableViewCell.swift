7ca7a50df01cc0985bd159b16c59b16d414feac0
Zack Larsen <zacklarsen1@gmail.com>
2019-12-16 12:39:08 -0700

    working on the ui

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Zack Larsen on 12/16/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var bodyText: UILabel!
+    @IBOutlet weak var timeStamp: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Zack Larsen on 12/16/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
