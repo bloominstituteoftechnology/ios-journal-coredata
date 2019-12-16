0a54adea65fb39e4ef6267115a387b3cc23b7a38
Tyler Christian <tylerc71197@gmail.com>
2019-12-16 15:38:32 -0500

    layouts done

---
+++ Journal/Journal/EntryDetialViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EntryDetialViewController.swift
+//  Journal
+//
+//  Created by Lambda_School_Loaner_218 on 12/16/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetialViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Lambda_School_Loaner_218 on 12/16/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+}
