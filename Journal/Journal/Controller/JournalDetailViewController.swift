a242ac5c464ddb14011f3026b029cfaa185adf3b
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-16 15:00:45 -0500

    Initial Commit and Project Structure. Also set up tableView

---
+++ Journal/Journal/Controller/JournalDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JournalDetailViewController.swift
+//  Journal
+//
+//  Created by Chad Rutherford on 12/16/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+    
+}
---
+++ Journal/Journal/Controller/JournalTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  JournalTableViewController.swift
+//  Journal
+//
+//  Created by Chad Rutherford on 12/16/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+class JournalTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Journal"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddTapped))
+    }
+    
+    @objc private func handleAddTapped() {
+        let journalDetailVC = JournalDetailViewController()
+        navigationController?.pushViewController(journalDetailVC, animated: true)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.journalCell, for: indexPath)
+        return cell
+    }
+}
+
+struct JournalPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func updateUIViewController(_ uiViewController: JournalPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<JournalPreview.ContainerView>) {
+            
+        }
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<JournalPreview.ContainerView>) -> UIViewController {
+            return UINavigationController(rootViewController: JournalTableViewController())
+        }
+    }
+}
