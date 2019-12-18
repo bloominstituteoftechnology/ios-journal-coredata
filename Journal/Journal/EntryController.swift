b825dcf7e97d2fa71c120b9b8109ecc3a70c7b01
Tyler Christian <tylerc71197@gmail.com>
2019-12-18 16:32:40 -0500

    Finished methods for persistent data on firebase

--- Journal/Journal/EntryController.swift
+++ Journal/Journal/EntryController.swift
@@ -9,7 +9,52 @@
 import CoreData
 import Foundation
 
+enum HTTPMethood: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
 class EntryController {
+    
+    typealias CompletionHandler = (Error?) -> Void
+    private let baseURL = URL(string: "https://journal-dca4e.firebaseio.com/")
+    
+    func put(entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        let identifire = entry.identifier ?? UUID().uuidString
+        guard let requestURL = baseURL?.appendingPathComponent(identifire).appendingPathExtension("json") else { return }
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethood.put.rawValue
+        
+        let encoder = JSONEncoder()
+        do {
+            guard var representation = entry.entryRepresentation else {
+                completion(NSError())
+                return
+            }
+            representation.identifier = identifire
+            entry.identifier = identifire
+            try saveToPersistentStore()
+            request.httpBody = try encoder.encode(representation)
+        } catch let encodeError {
+            print("Error encoding entry: \(encodeError.localizedDescription)")
+            completion(encodeError)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error putting entry to server: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    completion(error)
+                    return
+                }
+            }
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
     
     var entries: [Entry] {
        return loadFromPersistentStore()
@@ -36,7 +81,8 @@ class EntryController {
     }
     
     func createEntry(title: String, bodyText: String, mood:String) {
-        let _ = Entry(title: title, bodyText: bodyText, mood: mood)
+        let entry = Entry(title: title, bodyText: bodyText, mood: mood)
+        put(entry: entry)
         saveToPersistentStore()
     }
     
@@ -45,11 +91,13 @@ class EntryController {
         entry.bodytext = bodyText
         entry.timestamp = Date()
         entry.mood = mood
+        put(entry: entry)
         saveToPersistentStore()
     }
     
     func delete(entry: Entry) {
         let moc = CoreDataStack.shared.mainContext
+        deleteEntryFromServer(entry)
         moc.delete(entry)
         do {
             try moc.save()
@@ -57,5 +105,24 @@ class EntryController {
             moc.reset()
             print("Error deleting entry \(deleteError.localizedDescription)")
         }
+    }
+    
+    func deleteEntryFromServer(_ entry: Entry, completion: @escaping CompletionHandler = {_ in}) {
+        guard let identifier = entry.identifier, let requestURL = baseURL?.appendingPathComponent(identifier).appendingPathExtension("json") else { return }
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethood.delete.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                print("Error deleteing entry from server: \(error.localizedDescription)")
+                DispatchQueue.main.async{
+                completion(error)
+                return
+              }
+            }
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
     }
 }
