29774770e2e9a4c0f77144071071cdf3ac912af9
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-18 15:53:39 -0500

    Finished Entry Representation

--- Journal/Journal/Extensions/Entry+Convenience.swift
+++ Journal/Journal/Extensions/Entry+Convenience.swift
@@ -20,12 +20,26 @@ enum EntryMood: String {
 }
 
 extension Entry {
-    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: String = "😀", context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    
+    var entryRepresentation: EntryRepresentation? {
+        guard let title = title,
+            let timestamp = timestamp,
+            let identifier = identifier,
+            let mood = mood else { return nil }
+        return EntryRepresentation(title: title, bodyText: bodyText, timestamp: timestamp, identifier: identifier, mood: mood)
+    }
+    
+    @discardableResult convenience init(title: String, bodyText: String?, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: String = "😀", context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
         self.timestamp = timestamp
         self.identifier = identifier
         self.mood = mood
+    }
+    
+    @discardableResult convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let mood = EntryMood(rawValue: entryRepresentation.mood) else { return nil }
+        self.init(title: entryRepresentation.title, bodyText: entryRepresentation.bodyText, timestamp: entryRepresentation.timestamp, identifier: entryRepresentation.identifier, mood: mood.rawValue, context: context)
     }
 }
