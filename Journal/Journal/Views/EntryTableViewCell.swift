619679d4259c388acbe09ae9ec248661579e0f30
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-27 21:46:16 +0000

    EntriesTableViewControlleer and EntryTableViewCell done

---
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Tobi Kuyoro on 27/01/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
