04a248e7893be1d7f1e9a60e8b0867cd8ec75658
David Wright <davidtwright89@gmail.com>
2020-02-12 02:29:30 -0500

    Created a new Xcode project and setup the storyboard layout

---
+++ Journal (Core Data)/Journal (Core Data)/EntryDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EntryDetailViewController.swift
+//  Journal (Core Data)
+//
+//  Created by David Wright on 2/12/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
