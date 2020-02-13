d27f81114893970d148a48185adce1ae30e98807
sal562 <salmanamer@gmail.com>
2020-02-12 19:46:41 -0800

    finished DataModel and organization of files

---
+++ Journal/Journal/Model/CoreDataStack.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Sal Amer on 2/12/20.
+//  Copyright © 2020 Sal Amer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataStack {
+    static let shared = CoreDataStack()  //create singleton
+    
+    private init() {}
+    
+    lazy var container: NSPersistentContainer = { // create persistent store and persistant store container - Plural
+        let newContainer = NSPersistentContainer(name: "Entries")
+        newContainer.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent store: \(error)")
+            }
+        }
+        return newContainer
+    }()
+    var mainContext: NSManagedObjectContext { // created mamaged object context 
+        return container.viewContext
+    }
+}
