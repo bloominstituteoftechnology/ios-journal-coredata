63e7f1591e5b08020d80a8bdc5223e282fa9e693
Gerardo <gahernandez02@gmail.com>
2020-02-12 19:22:46 -0800

    Added Entry extension

---
+++ Journal/Journal/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Gerardo Hernandez on 2/12/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Gerardo Hernandez on 2/12/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
