92c256c7167b84fbd229b206c5f335310e73365d
David Wright <davidtwright89@gmail.com>
2020-02-18 02:20:17 -0500

    Updated EntryController to enable PUTting and deleting entries to the server

--- Journal (Core Data)/Journal (Core Data)/Model/EntryController.swift
+++ Journal (Core Data)/Journal (Core Data)/Model/EntryController.swift
@@ -23,11 +23,76 @@ class EntryController {
     typealias CompletionHandler = (Error?) -> Void
     
     // MARK: - CRUD Methods
+    
+    private func putEntryToServer(_ entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        let uuidString = entry.identifier ?? UUID().uuidString
+        let requestURL = baseURL.appendingPathComponent(uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.dateEncodingStrategy = .iso8601
+        do {
+        guard var representation = entry.entryRepresentation else {
+                completion(NSError())
+                return
+            }
+            representation.identifier = uuidString
+            entry.identifier = uuidString
+            saveToPersistentStore()
+            request.httpBody = try jsonEncoder.encode(representation)
+        } catch {
+            print("Error encoding entry: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                print("Error PUTting entry to server: \(error!)")
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    private func deleteEntryFromServer(_ entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        guard let uuidString = entry.identifier else {
+            completion(NSError())
+            return
+        }
+        
+        let requestURL = baseURL.appendingPathComponent(uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.delete.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            guard error == nil else {
+                print("Error deleting entry: \(error!)")
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    // MARK: - CRUD Methods
 
     // Create Entry
     func createEntry(withTitle title: String, bodyText: String, mood: String) {
-        Entry(title: title, bodyText: bodyText, mood: mood)
-        saveToPersistentStore()
+        let entry = Entry(title: title, bodyText: bodyText, mood: mood)
+        putEntryToServer(entry)
     }
     
     // Update Entry
@@ -37,14 +102,22 @@ class EntryController {
         entry.bodyText = updatedBodyText
         entry.timestamp = updatedTimestamp
         entry.mood = updatedMood
-        saveToPersistentStore()
+        putEntryToServer(entry)
     }
     
     // Delete Entry
     func deleteEntry(_ entry: Entry) {
-        let moc = CoreDataStack.shared.mainContext
-        moc.delete(entry)
-        saveToPersistentStore()
+        deleteEntryFromServer(entry) { (error) in
+            guard error == nil else {
+                print("Error deleting entry from server: \(error!)")
+                return
+            }
+            
+            let moc = CoreDataStack.shared.mainContext
+            moc.delete(entry)
+            
+            self.saveToPersistentStore()
+        }
     }
     
     // MARK: - Persistence
