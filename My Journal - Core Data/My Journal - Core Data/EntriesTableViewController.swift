612867f1b4f92ebe884c7cc6e1a014317bc44157
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-24 15:47:36 -0500

    Set up title and segue

---
+++ My Journal - Core Data/My Journal - Core Data/EntriesTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EntriesTableViewController.swift
+//  My Journal - Core Data
+//
+//  Created by Nick Nguyen on 2/24/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    // MARK: - Table view data source
+
+ 
+
+}
