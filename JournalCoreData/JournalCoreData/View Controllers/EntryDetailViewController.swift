96a82a3c5f56503f8319bd5b02fb16f496d8d714
scottharris86 <scottharrisdev@gmail.com>
2020-02-24 17:46:48 -0500

    detail view controller implemented

---
+++ JournalCoreData/JournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EntryDetailViewController.swift
+//  JournalCoreData
+//
+//  Created by scott harris on 2/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+}
