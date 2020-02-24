a07fbfa833a301aafdf91a252107474bfa920818
scottharris86 <scottharrisdev@gmail.com>
2020-02-24 17:51:41 -0500

    created core data stack

---
+++ JournalCoreData/JournalCoreData/Models/CoreDataStack.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataStack.swift
+//  JournalCoreData
+//
+//  Created by scott harris on 2/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+       let container = NSPersistentContainer(name: "Entry")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Error loading persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+}
