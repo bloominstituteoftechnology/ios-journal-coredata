24c6dc29fbcd3b1e356f54a724ba726a5540c084
Enrique Gongora <EGTech24@gmail.com>
2020-02-24 13:27:20 -0800

    Basic setup done

---
+++ JournalCoreData/JournalCoreData/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  JournalCoreData
+//
+//  Created by Enrique Gongora on 2/24/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+}
