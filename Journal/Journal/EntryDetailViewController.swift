89930fa39be915acaaaf850ad8e86549381c5096
Chris <chris.gonzales.82@gmail.com>
2020-02-24 12:57:11 -0800

    Created EntryDetailVC, wried up storyboard

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Chris Gonzales on 2/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet var entryTitleLabel: UITextField!
+    @IBOutlet var entryDescriptionText: UITextView!
+    
+    @IBAction func saveTapped (_ sender: UIBarButtonItem){
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
