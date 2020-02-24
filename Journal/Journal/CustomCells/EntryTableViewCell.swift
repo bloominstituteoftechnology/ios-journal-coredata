9297cb6cf6853c48d00a117e7016d994942ddd4a
Chris <chris.gonzales.82@gmail.com>
2020-02-24 12:43:53 -0800

    Journal storyboard, JournalTableVC, EnterTableViewCell created.  Wired up storyboard elements.  Created and named segures.

---
+++ Journal/Journal/CustomCells/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Chris Gonzales on 2/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
