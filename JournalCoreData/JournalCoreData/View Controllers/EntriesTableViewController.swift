5dc6959f6c7a4019ae8758737cece1b46d3cd4a4
scottharris86 <scottharrisdev@gmail.com>
2020-02-24 17:07:43 -0500

    project setup entries tableview controller entry table view cell

---
+++ JournalCoreData/JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EntriesTableViewController.swift
+//  JournalCoreData
+//
+//  Created by scott harris on 2/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+    
+}
---
+++ JournalCoreData/JournalCoreData/Views/Cells/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  JournalCoreData
+//
+//  Created by scott harris on 2/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    
+}
