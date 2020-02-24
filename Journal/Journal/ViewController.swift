9402bb5d755a0399b85d3d82871a82b9e64ff615
Chris <chris.gonzales.82@gmail.com>
2020-02-24 12:25:19 -0800

    Initial commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Chris Gonzales on 2/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
