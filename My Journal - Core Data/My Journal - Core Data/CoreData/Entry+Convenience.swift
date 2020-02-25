8d6a57795dd1b0221535ee8c1c5b35372b978036
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-25 15:48:59 -0500

    Update CoreData model

--- My Journal - Core Data/My Journal - Core Data/CoreData/Entry+Convenience.swift
+++ My Journal - Core Data/My Journal - Core Data/CoreData/Entry+Convenience.swift
@@ -10,10 +10,18 @@ import Foundation
 import CoreData
 
  extension Entry {
-     convenience init(title: String, bodyText: String ,timestamp: Date = Date(), identifier : String = "", context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+     convenience init(title: String,
+                      bodyText: String ,
+                      timestamp: Date = Date(),
+                      identifier : String = "",
+                      context: NSManagedObjectContext = CoreDataStack.shared.mainContext,
+                      mood: String)
+     {
         self.init(context:context)
         self.title = title
         self.bodyText = bodyText
-        
+        self.mood = mood
     }
+    
+    
 }
