04f88512c971f8ebbf5f7e1ee44519545519a903
Enrique Gongora <EGTech24@gmail.com>
2020-02-24 17:22:38 -0800

    UpdateViews Added

--- JournalCoreData/JournalCoreData/EntryTableViewCell.swift
+++ JournalCoreData/JournalCoreData/EntryTableViewCell.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
     
+    //MARK: - IBOutlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyTextLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
+    
+    //MARK: - Properties
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    //MARK: - UpdateViews Function
+    func updateViews() {
+        if let entry = entry {
+            titleLabel.text = entry.title
+            bodyTextLabel.text = entry.bodyText
+            timestampLabel.text = dateFormatter.string(from: entry.timestamp ?? Date())
+        }
+    }
 }
