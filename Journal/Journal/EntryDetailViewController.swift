0e9e1db29ab7a183e715cbd744f4c4828d578ef2
Chris <chris.gonzales.82@gmail.com>
2020-02-24 16:04:25 -0800

    MVP

--- Journal/Journal/EntryDetailViewController.swift
+++ Journal/Journal/EntryDetailViewController.swift
@@ -42,6 +42,7 @@ class EntryDetailViewController: UIViewController {
             entryController.createEntry(title: titleTextField,
                                         bodyText: descriptionTextField)
         }
+        navigationController?.popViewController(animated: true)
     }
     
     
