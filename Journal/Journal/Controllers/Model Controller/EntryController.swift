2ebcf51d91760a1c8536dd4f490a817795682bf0
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-26 22:07:10 +0000

    EntryController Put Method

--- Journal/Journal/Controllers/Model Controller/EntryController.swift
+++ Journal/Journal/Controllers/Model Controller/EntryController.swift
@@ -9,7 +9,59 @@
 import Foundation
 import CoreData
 
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
 class EntryController {
+    
+    let baseURL = URL(string: "https://journal-ios14.firebaseio.com/")!
+    
+    typealias CompletionHandler = (Error?) -> Void
+    
+    // MARK: - Networking Methods
+    
+    func put(entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        guard let identifier = entry.identifier else { return }
+        entry.identifier = identifier
+        
+        let requestURL = baseURL
+            .appendingPathComponent(identifier)
+            .appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        guard let entryRepresentation = entry.entryRepresentation else {
+            NSLog("There is no entry representation")
+            completion(NSError())
+            return
+        }
+        
+        do {
+            let entryData = try JSONEncoder().encode(entryRepresentation)
+            request.httpBody = entryData
+        } catch {
+            NSLog("Error encoding entry representation: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, _, error in
+            if let error = error {
+                NSLog("Error storing entries in Firebase: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    // MARK: - Core Data Methods
     
     func saveToPersistentStore() {
         do {
@@ -29,6 +81,7 @@ class EntryController {
                           identifier: identifier,
                           mood: mood,
                           context: CoreDataTask.shared.mainContext)
+        put(entry: entry)
         saveToPersistentStore()
         return entry
     }
@@ -39,6 +92,7 @@ class EntryController {
         entry.timeStamp = timeStamp
         entry.identifier = identifier
         entry.mood = mood
+        put(entry: entry)
         saveToPersistentStore()
     }
     
