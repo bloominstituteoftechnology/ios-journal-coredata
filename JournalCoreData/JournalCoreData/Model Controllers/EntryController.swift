e09bde0eddcbe14b2216de4cc6291ff62431d645
scottharris86 <scottharrisdev@gmail.com>
2020-02-26 20:19:37 -0500

    completed syncing

--- JournalCoreData/JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/JournalCoreData/Model Controllers/EntryController.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class EntryController {
     
-    let baseURL = URL(string: "https://journal-core-data-6cb21.firebaseio.com")!
+    let baseURL = URL(string: "https://journal-core-data-6cb21.firebaseio.com/")!
     
 //    var entries: [Entry] {
 //        return loadFromPersistentStore()
@@ -35,6 +35,10 @@ class EntryController {
 //        }
 //        
 //    }
+    
+    init() {
+        fetchEntriesFromServer()
+    }
     
     func createEntry(title: String, body: String, mood: String) {
         let entry = Entry(title: title, bodyText: body, timestamp: Date(), mood: mood, identifier: UUID().uuidString)
@@ -116,6 +120,76 @@ class EntryController {
             
         }.resume()
         
+    }
+    
+    func update(entry: Entry, entryRepresentation: EntryRepresentation) {
+        Entry(entryRepresentation: entryRepresentation)
+        
+    }
+    
+    func updateEntries(with representations: [EntryRepresentation]) {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        
+        let entriesWithID = representations.filter { $0.identifier != nil }
+        let identifiersToFetch = entriesWithID.compactMap { $0.identifier }
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, entriesWithID))
+        var entriesToCreate = representationsByID
+        
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+        do {
+            let existingEntries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+            
+            // match the managed entries with the firebase entries
+            for entry in existingEntries {
+                guard let id = entry.identifier, let representation = representationsByID[id] else { continue }
+                self.update(entry: entry, entryRepresentation: representation)
+                entriesToCreate.removeValue(forKey: id)
+            }
+            
+            for representation in entriesToCreate.values {
+                Entry(entryRepresentation: representation)
+            }
+            
+            saveToPersistentStore()
+            
+        } catch {
+            NSLog("Error fetching entries for UUIS's: \(error)")
+        }
+        
+    }
+    
+    func fetchEntriesFromServer(completion: @escaping (Error?) -> Void = { _ in }) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        print(requestURL)
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error fetching entires from firebase: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error with data from firebase:")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let entryRepresentations = Array(try JSONDecoder().decode([String: EntryRepresentation].self, from: data).values)
+                
+                self.updateEntries(with: entryRepresentations)
+                
+                completion(nil)
+                
+            } catch {
+                NSLog("Error decoding entry representations from firebase: \(error)")
+                completion(error)
+            }
+        }.resume()
         
     }
     
