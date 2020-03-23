85dc70d18805217476b30a4e05b8791117e1d715
Shawn Gee <shawngee@gmail.com>
2020-03-23 15:51:59 -0400

    EntryDetailViewController laid out and hooked up

--- Journal/Journal/EntryDetailViewController.swift
+++ Journal/Journal/EntryDetailViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class EntryDetailViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleTextField.becomeFirstResponder()
+        setBorder(for: titleTextField, bodyTextView)
+       
+        
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Private
+    
+    private func setBorder(for views: UIView...) {
+        views.forEach {
+            $0.layer.borderColor = UIColor.systemGray3.cgColor
+            $0.layer.borderWidth = 1.0
+            $0.layer.cornerRadius = 6.0
+        }
     }
 
 
