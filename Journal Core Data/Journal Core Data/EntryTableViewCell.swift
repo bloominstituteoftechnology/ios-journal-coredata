9b8769602c6152f5ec4b05a7ca36e8e0b0d2dfe1
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-23 12:46:36 -0700

    assigned the classes to the tableview controller

---
+++ Journal Core Data/Journal Core Data/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal Core Data
+//
+//  Created by Bhawnish Kumar on 3/23/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
