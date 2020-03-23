edc82f08323a56dbaae6d01e00337630ceebdce0
Shawn Gee <shawngee@gmail.com>
2020-03-23 16:09:14 -0400

    Core data model, stack, and convenience extension created

---
+++ Journal/Journal/CoreData/Entry+Convenience.swift
@@ -0,0 +1,25 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Shawn Gee on 3/23/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    @discardableResult
+    convenience init(title: String,
+                     bodyText: String?,
+                     timestamp: Date = Date(),
+                     identifier: String = UUID().uuidString,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
