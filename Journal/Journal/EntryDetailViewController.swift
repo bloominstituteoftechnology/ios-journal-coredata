cc32de26098e5f47f61c131f3ef04031841f9983
Shawn Gee <shawngee@gmail.com>
2020-03-23 15:33:54 -0400

    Storyboard laid out, entry cell outlets hooked up.

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Shawn Gee on 3/23/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Shawn Gee on 3/23/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+}
