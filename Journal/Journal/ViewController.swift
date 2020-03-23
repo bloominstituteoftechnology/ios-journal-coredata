2df714c9a4670b6f97e377b3de7bdb64839d5eb9
Mark Gerrior <github@gerrior.com>
2020-03-23 14:00:32 -0700

    Added Xcode project with some reorg

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Mark Gerrior on 3/23/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
