071a9aa66b7bc48959bd1ab75100b96eb3c97129
Mark Gerrior <github@gerrior.com>
2020-03-23 15:37:04 -0700

    EntryDetailViewController Completed

---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Mark Gerrior on 3/23/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    // MARK: - Properities
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var entryTextView: UITextView!
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Actions
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+    }
+
+}
+
