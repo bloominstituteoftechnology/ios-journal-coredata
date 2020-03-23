e3ddce3f97ba2ed2a807c2190b6f6f3c630a970b
Brad <braddiroff@icloud.com>
2020-03-23 15:13:09 -0400

    Added project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Bradley Diroff on 3/23/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
