162fa5ad9ecdabc9f0124acacc40c9d017746571
Hector L <hectoremil21@hotmail.com>
2020-03-23 16:22:00 -0400

    EntryDetailVC setup. Constrained and Outlets.

---
+++ journal-coredata/journal-coredata/EntryDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EntryDetailViewController.swift
+//  journal-coredata
+//
+//  Created by Karen Rodriguez on 3/23/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    // MARK: -  Outlets
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK: - Button Action
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        
+    }
+
+}
+
