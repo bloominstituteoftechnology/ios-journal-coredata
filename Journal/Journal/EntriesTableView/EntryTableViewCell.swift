244984d01ce316dd849744cf8fea0108745dda13
Shawn Gee <shawngee@gmail.com>
2020-03-23 16:37:27 -0400

    Added entry property and updateViews function to EntryTableViewCell

--- Journal/Journal/EntriesTableView/EntryTableViewCell.swift
+++ Journal/Journal/EntriesTableView/EntryTableViewCell.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
     
+    // MARK: - Properties
+    
+    var entry: Entry? { didSet { updateViews() }}
+    
     // MARK: - IBOutlets
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
+    
+    
+    // MARK: - Private
+    
+    func updateViews() {
+        guard let entry = entry else { return }
+        titleLabel.text = entry.title
+        bodyLabel.text = entry.bodyText
+        timestampLabel.text = entry.timestamp?.shortString
+    }
+}
+
+extension Date {
+    var shortString: String {
+        return DateFormatter.shortFormatter.string(from: self)
+    }
+}
+
+extension DateFormatter {
+    static var shortFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        return dateFormatter
+    }
 }
