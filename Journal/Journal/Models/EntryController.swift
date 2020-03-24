59fedd06f7ddafb952f6ad5fe40bd9d44b86a87f
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-24 17:21:49 -0400

    part two complete

--- Journal/Journal/Models/EntryController.swift
+++ Journal/Journal/Models/EntryController.swift
@@ -11,9 +11,9 @@ import CoreData
 
 class EntryController {
     
-    var entries: [Entry] {
-        loadFromPersistentStore()
-    }
+//    var entries: [Entry] {
+//        loadFromPersistentStore()
+//    }
     
     func saveToPersistentStore() {
         do {
@@ -40,14 +40,14 @@ class EntryController {
         CoreDataStack.shared.mainContext.delete(entry)
     }
 
-    func loadFromPersistentStore() -> [Entry] {
-        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        do {
-            return try CoreDataStack.shared.mainContext.fetch(fetchRequest)
-        } catch {
-            NSLog("Error saving managed object contect: \(error)")
-            return []
-        }
-    }
+//    func loadFromPersistentStore() -> [Entry] {
+//        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+//        do {
+//            return try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+//        } catch {
+//            NSLog("Error saving managed object contect: \(error)")
+//            return []
+//        }
+//    }
     
 }
--- Journal/Journal/View Controllers/EntriesTableViewController.swift
+++ Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class EntriesTableViewController: UITableViewController {
 
     let entryController = EntryController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "mood", ascending: true),
+        NSSortDescriptor(key: "timestamp", ascending: true)]
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "mood", cacheName: nil)
+        
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +35,28 @@ class EntriesTableViewController: UITableViewController {
         if let selectedRow = tableView.indexPathForSelectedRow {
             tableView.deselectRow(at: selectedRow, animated: true)
         }
-        
-        tableView.reloadData()
     }
-    
     // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        fetchedResultsController.sections?.count ?? 1
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return entryController.entries.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil }
+        
+        return sectionInfo.name
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath) as? EntryTableViewCell else { return UITableViewCell() }
 
-        cell.entry = entryController.entries[indexPath.row]
+        let entry = fetchedResultsController.object(at: indexPath)
+        cell.entry = entry
 
         return cell
     }
@@ -43,9 +64,20 @@ class EntriesTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let entry = entryController.entries[indexPath.row]
-            entryController.delete(entry: entry)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            let entry = fetchedResultsController.object(at: indexPath)
+            let context = CoreDataStack.shared.mainContext
+            context.delete(entry)
+            
+            do {
+                try context.save()
+            } catch {
+                context.reset()
+                NSLog("Error saving managed object context. Could not delete record.\(error)")
+            }
+            
+            
+            //tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
@@ -56,7 +88,7 @@ class EntriesTableViewController: UITableViewController {
             guard let EntryDetailVC = Nav.topViewController as? EntryDetailViewController else { return }
             guard let selected = tableView.indexPathForSelectedRow else { return }
             EntryDetailVC.entryController = entryController
-            EntryDetailVC.entry = entryController.entries[selected.row]
+            EntryDetailVC.entry = fetchedResultsController.object(at: selected)
         } else if segue.identifier == "NewEntryModalSegue" {
             guard let Nav = segue.destination as? UINavigationController else { return }
             guard let NewEntryVC = Nav.topViewController as? EntryDetailViewController else { return }
@@ -66,3 +98,53 @@ class EntriesTableViewController: UITableViewController {
     
 
 }
+
+extension EntriesTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
+}
+
