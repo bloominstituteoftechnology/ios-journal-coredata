f3ae164d2ce06072bb79b3032f12654e20d7bd46
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-24 16:01:42 -0700

    Added header to the table view

--- Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
+++ Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
@@ -49,15 +49,11 @@ class EntriesTableViewController: UITableViewController {
         return cell
     }
    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+         guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil}
+               
+               return sectionInfo.name.capitalized
     }
-    */
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
