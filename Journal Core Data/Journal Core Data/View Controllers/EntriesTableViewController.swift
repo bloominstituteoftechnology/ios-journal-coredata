100ac60bf1d6ac497c359e34db2435e9af713e99
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-24 15:22:11 -0700

    Added the fetch request controller to the table view

--- Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
+++ Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import CoreData
 class EntriesTableViewController: UITableViewController {
 
     var entryController = EntryController()
@@ -24,7 +24,17 @@ class EntriesTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
- 
+ lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "mood", ascending: true),
+        NSSortDescriptor(key: "timeStamp", ascending: true)]
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "mood", cacheName: nil)  // we are gathering everything in this  featchrequest, manageobjejcrt contextr and everything from what we created in the constants.
+        frc.delegate = self
+        try! frc.performFetch()
+         return frc
+          
+    }()
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -85,3 +95,54 @@ class EntriesTableViewController: UITableViewController {
 
 
 }
+
+extension EntriesTableViewController: NSFetchedResultsControllerDelegate {
+    //     this is the warning the tableview that the fetch controller is goijng to makechanges in the tableview.
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+        //        this is the beggnining of the fetchhing
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+        //         the endo of the fetchhing.
+    }
+    //    deletes the entire section or insert entire section
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
+}
+
