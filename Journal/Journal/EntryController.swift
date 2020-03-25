027ca4b7e102f6f18fb4b2c818eb325152ea9432
Mark Gerrior <github@gerrior.com>
2020-03-25 16:03:15 -0700

    Part 2 - Syncing Databases Steps 1 - 2

--- Journal/Journal/EntryController.swift
+++ Journal/Journal/EntryController.swift
@@ -122,6 +122,59 @@ class EntryController {
         put(entry: entry)
     }
 
+    private func update(entry: Entry, with representation: EntryRepresentation) {
+        //entry.identifier = representation.identifier
+        entry.title = representation.title
+        entry.bodyText = representation.bodyText
+        entry.timestamp = representation.timestamp
+        entry.mood = representation.mood
+    }
+
+    /// <#Description#>
+    /// - Parameter representations: EntryRepresentation objects that are fetched from Firebase
+    private func updateEntries(with representations: [EntryRepresentation]) throws {
+        
+        /// Create a fetch request from Entry object.
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+
+        /// Create a dictionary with the identifiers of the representations as the keys, and the values as the representations. To accomplish making this dictionary you will need to create a separate array of just the entry representations identifiers. You can use the zip method to combine two arrays of items together into a dictionary.
+        let entriesByID = representations.filter { !$0.identifier.isEmpty }
+        let identifiersToFetch = entriesByID.compactMap { $0.identifier }
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, entriesByID))
+        var entriesToCreate = representationsByID
+
+        /// Give the fetch request an NSPredicate. This predicate should see if the identifier attribute in the Entry is in identifiers array that you made from the previous step. Refer to the hint below if you need help with the predicate.
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+
+        /// Perform the fetch request on your core data stack's mainContext.
+        let context = CoreDataStack.shared.mainContext
+        
+        do {
+            /// This will return an array of Entry objects whose identifier was in the array you passed in to the predicate.
+            let existingEntries = try context.fetch(fetchRequest)
+            
+            /// Loop through the fetched entries and call update. Then remove the entry from the dictionary. Afterwards we'll create entries from the remaining objects in the dictionary. The only ones that would remain after this loop are ones that didn't exist in Core Data already.
+            for entry in existingEntries {
+                guard let id = entry.identifier,
+                    let representation = representationsByID[id] else { continue }
+                self.update(entry: entry, with: representation)
+                entriesToCreate.removeValue(forKey: id)
+            }
+            
+            /// Create an entry for each of the values in entriesToCreate using the Entry initializer that takes in an EntryRepresentation and an NSManagedObjectContext
+            for representation in entriesToCreate.values {
+                Entry(entryRepresentation: representation, context: context)
+            }
+        } catch {
+            /// Make sure you handle a potential error from the fetch method on your managed object context, as it is a throwing method.
+            NSLog("Error fetching tasks for UUIDs: \(error)")
+        }
+        
+        // TODO: ? This isn't under both loops. Concerned about saving too much
+        /// Under both loops, call saveToPersistentStore() to persist the changes and effectively synchronize the data in the device's persistent store with the data on the server. Since you are using an NSFetchedResultsController, as soon as you save the managed object context, the fetched results controller will observe those changes and automatically update the table view with the updated entries.
+        try context.save() // Caller will handle
+    }
+    
     // Delete
     func delete(entry: Entry) {
 
@@ -134,6 +187,6 @@ class EntryController {
         
         saveToPersistentStore()
         
-        // FIXME: Firebase delete
+        // FIXME: Firebase delete. EntryController Step 4 and 5.
     }
 }
