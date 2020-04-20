ca5ecbf78ee4547452e886664720aa730bc93c58
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-20 13:01:52 -0700

    Part1: Added files

---
+++ JournalCoreData/JournalCoreData/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  JournalCoreData
+//
+//  Created by Bhawnish Kumar on 4/20/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
