ef87ad770908b9436e121f81eb4f00fa301472b4
Mark Poggi <markp@mac.com>
2020-04-20 12:34:57 -0700

    Journal v1

---
+++ Journal/Journal/Models/Core Data/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Mark Poggi on 4/20/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Mark Poggi on 4/20/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+import Foundation
