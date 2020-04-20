d7bad6d094ecc677365277c38135df784379c211
Harmony Radley <harmonyradley99@gmail.com>
2020-04-20 17:29:18 -0400

    Entered Code for CoreDataStack

--- Journal/Journal/CoreDataStack.swift
+++ Journal/Journal/CoreDataStack.swift
@@ -11,7 +11,20 @@ import CoreData
 
 class CoreDataStack {
     
-static let shared = CoreDataStack()
-
-
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
 }
--- Journal/Journal/Entry+Convenience.swift
+++ Journal/Journal/Entry+Convenience.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreData
 
 extension Entry {
-    @discardableResuilt convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    @discardableResult convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
