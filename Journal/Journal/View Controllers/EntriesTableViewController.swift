b25634d2d6baf31ad2c2ef369707ccc1a94b1304
Cameron Collins <cameroncollins19@yahoo.com>
2020-04-21 17:46:08 -0500

    Working on ShowViewController

--- Journal/Journal/View Controllers/EntriesTableViewController.swift
+++ Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -84,32 +84,22 @@ class EntriesTableViewController: UITableViewController {
         }
     }
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //"createSegue"
+        //"editSegue"
+        
+        guard let identifier = segue.identifier else { return }
+        
+        if identifier == "editSegue" {
+            if let detailVC = segue.destination as? ShowEntryViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                //detailVC.entry = fetchedResultsController.object(at: indexPath)
+            }
+        }
     }
-    
-
 }
 
 
