eee0bfc855f53ab515b92720e540c2163ceefbe3
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-21 10:58:20 -0400

    progress

--- Journal/Journal/Models/Core Data/CoreDataStack.swift
+++ Journal/Journal/Models/Core Data/CoreDataStack.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    //  Singleton - Everyone will get the same shared instance bc of the "static let" no matter where you are
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Journal") // The name of the container needs to match the name of the Data Model you're going to use/name of your app
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persisten stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
+
--- Journal/Journal/Views/EntryTableViewCell.swift
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
+    var entry: Entry?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
