c578d3dad9dba896237b34422faf5cc73251281a
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-21 10:11:30 -0400

    not finished

---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Dahna on 4/20/20.
+//  Copyright © 2020 Dahna Buenrostro. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/View Controllers/CreateEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Dahna on 4/20/20.
+//  Copyright © 2020 Dahna Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class CreateEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
