d0e0159c78c1fc1afb5c9dd1991282e005dcc09c
David Williams <burmesedragon@att.net>
2020-04-22 15:56:11 -0700

    recreated storyboard.  need to recode the rest of the files

--- Journal/Journal/CoreDataStack.swift
+++ Journal/Journal/CoreDataStack.swift
@@ -6,24 +6,4 @@
 //  Copyright © 2020 david williams. All rights reserved.
 //
 
-import CoreData
-
-class CoreDataStack {
-    
-    static let shared = CoreDataStack()
-    
-    lazy var container: NSPersistentContainer = {
-   
-        let container = NSPersistentContainer(name: kCFBundleNameKey as String)
-        container.loadPersistentStores { (_, error) in
-            if let error = error {
-            fatalError("Fail to load persistent stores: \(error)")
-            }
-        }
-        return container
-    }()
-    
-    var mainContext: NSManagedObjectContext {
-        return container.viewContext
-    }
-}
+import Foundation
