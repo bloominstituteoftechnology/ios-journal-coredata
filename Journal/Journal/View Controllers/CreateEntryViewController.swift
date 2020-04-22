c9261ea5078926bdfa7a0892faa231961a812b89
Matt Martindale <martindalematt18@yahoo.com>
2020-04-21 22:41:06 -0600

    Setup tableView UI

--- Journal/Journal/View Controllers/CreateEntryViewController.swift
+++ Journal/Journal/View Controllers/CreateEntryViewController.swift
@@ -12,30 +12,10 @@ class CreateEntryViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var entryLabel: UILabel!
-    private var lineView: UIView = UIView()
-    private let viewSize: CGSize = CGSize(width: 40, height: 5)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setUp()
-    }
-
-    func setUp() {
-        lineView.translatesAutoresizingMaskIntoConstraints = false
-        lineView.backgroundColor = .gray
-
-        view.addSubview(lineView)
-
-        lineView.topAnchor.constraint(equalTo: entryLabel.bottomAnchor, constant: 12).isActive = true
-        lineView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15).isActive = true
-        lineView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
-        lineView.heightAnchor.constraint(equalToConstant: viewSize.height).isActive = true
-        lineView.widthAnchor.constraint(equalToConstant: viewSize.width).isActive = true
     }
 
 }
