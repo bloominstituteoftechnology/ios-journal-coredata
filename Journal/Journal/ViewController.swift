f5074b8353d1066aa722fe24c6bc4aa1f5a55745
Matt Martindale <martindalematt18@yahoo.com>
2020-04-21 21:53:45 -0600

    Initial commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Matthew Martindale on 4/21/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
