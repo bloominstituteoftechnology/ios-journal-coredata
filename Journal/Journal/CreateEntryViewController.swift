76a876a29de75ab2bf8a02c50940c3ded523601b
David Williams <burmesedragon@att.net>
2020-04-22 14:33:25 -0700

    initial commit with initial storyboard set up and files created for data model et al

---
+++ Journal/Journal/CreateEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by David Williams on 4/21/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Journal/Journal/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by David Williams on 4/21/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import Foundation
