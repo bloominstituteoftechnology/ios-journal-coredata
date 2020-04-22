d1e31fe8e30660d8b358f66298023762c00c6b4e
Jarren Campos <jarrenmcampos@gmail.com>
2020-04-22 13:56:13 -0700

    Inital setup and story board setup

---
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -0,0 +1,12 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Jarren Campos on 4/22/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
