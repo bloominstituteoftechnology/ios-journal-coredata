5e8c85b6b01dc91f0da4741be6f967e38a0d220d
Nichole Davidson <nicholedavid@gmail.com>
2020-04-21 22:56:26 -0600

    Added editButton and updateViews method

--- Journal - CoreData/Journal - CoreData/View Controllers/EntryDetailViewController.swift
+++ Journal - CoreData/Journal - CoreData/View Controllers/EntryDetailViewController.swift
@@ -10,29 +10,59 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
     
-
-   @IBOutlet weak var titleTextField: UITextField!
-      @IBOutlet weak var entryTextView: UITextView!
-      @IBOutlet weak var moodControl: UISegmentedControl!
-
+    // MARK: Properties
+    
+    var entry: Entry?
+    var wasEdited = false
+    
+     // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var entryTextView: UITextView!
+    @IBOutlet weak var moodControl: UISegmentedControl!
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = editButtonItem
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        titleTextField.text = entry?.title
+        titleTextField.isUserInteractionEnabled = isEditing
+        
+        entryTextView.text = entry?.bodyText
+        entryTextView.isUserInteractionEnabled = isEditing
+        
+        let mood: EntryMood
+        if let entryMood = entry?.mood {
+            mood = EntryMood(rawValue: entryMood)!
+        } else {
+            mood = .😐
+        }
+        
+        moodControl.selectedSegmentIndex = EntryMood.allCases.firstIndex(of: mood) ?? 1
+        moodControl.isUserInteractionEnabled = isEditing
     }
-    */
-
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
