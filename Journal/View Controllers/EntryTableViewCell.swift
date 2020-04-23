4b69b3c8a614f6bdeb01473547ce650fb39276fe
Chad Parker <chad@chadparker.com>
2020-04-23 16:48:47 -0700

    Fill out cell

--- Journal/View Controllers/EntryTableViewCell.swift
+++ Journal/View Controllers/EntryTableViewCell.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    }
+    
+    private func updateViews() {
+        guard
+            let entry = entry,
+            let date = entry.timestamp else { fatalError() }
+        
+        titleLabel.text = entry.title
+        dateLabel.text = dateFormatter.string(from: date)
+        bodyTextLabel.text = entry.bodyText
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-
 }
