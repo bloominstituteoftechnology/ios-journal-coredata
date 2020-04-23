59285f5faae1f401e3aeece70aa79f7e4a6c6807
James McDougall <jameswiliammcdougall@gmail.com>
2020-04-22 19:35:27 -0700

    Basic UI Setup

---
+++ Journal/Journal/Models/CoreData.swift
@@ -0,0 +1,9 @@
+//
+//  CoreData.swift
+//  Journal
+//
+//  Created by Lambda_School_loaner_226 on 4/22/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/Views/Cells/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Lambda_School_loaner_226 on 4/22/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
