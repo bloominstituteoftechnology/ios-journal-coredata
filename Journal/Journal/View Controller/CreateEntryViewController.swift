a2d75036b589e469b84a8929156ce89b5bdef907
Claudia Maciel <clc_80@yahoo.com>
2020-04-22 19:32:02 -0700

    TableView setup

--- Journal/Journal/View Controller/CreateEntryViewController.swift
+++ Journal/Journal/View Controller/CreateEntryViewController.swift
@@ -22,9 +22,23 @@ class CreateEntryViewController: UIViewController {
     
     // MARK: - IBAction
     @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = journalEntryTextField.text, !title.isEmpty,
+            let text = journalTextView.text, !text.isEmpty else { return }
+        
+        let currentDateTime = Date()
+        
+        Entry(identifier: "", title: title, bodyText: text, timestamp: currentDateTime, context: CoreDataStack.shared.mainContext)
+        
+        do {
+            try CoreDataStack.shared.mainContext.save()
+            navigationController?.dismiss(animated: true, completion: nil)
+        } catch {
+            NSLog("Error saving manage object context: \(error)")
+        }
     }
     
 
--- Journal/Journal/View Controller/EntriesTableViewController.swift
+++ Journal/Journal/View Controller/EntriesTableViewController.swift
@@ -7,60 +7,74 @@
 //
 
 import UIKit
+import CoreData
 
 class EntriesTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    var entries: [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        let context = CoreDataStack.shared.mainContext
+        
+        do {
+            let fetchTasks = try context.fetch(fetchRequest)
+            return fetchTasks
+        } catch {
+            NSLog("Error fetching tasks: \(error)")
+            return []
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return entries.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "journalTitleCell", for: indexPath) as? EntryTableViewCell else { return UITableViewCell() }
 
-        // Configure the cell...
-
+        let entry = entries[indexPath.row]
+        cell.titleLabel.text = entry.title
+        cell.excerptLabel.text = entry.bodyText
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .short
+        cell.dateAndTimeLabel.text = "\(dateFormatter.string(from: entry.timestamp ?? Date() ))"
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let entry = entries[indexPath.row]
+            let context = CoreDataStack.shared.mainContext
+            
+            context.delete(entry)
+            
+            do {
+                try context.save()
+            } catch {
+                NSLog("Error saving context after deleting Task: \(error)")
+                context.reset()
+            }
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
