c3d65cf9cb54e9e8138cfcd191760cbe702c8d28
Chad Parker <chad@chadparker.com>
2020-04-22 19:54:27 -0700

    Add Xcode project

---
+++ Journal/CreateEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateEntryViewController.swift
+//  Journal
+//
+//  Created by Chad Parker on 4/22/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class CreateEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
