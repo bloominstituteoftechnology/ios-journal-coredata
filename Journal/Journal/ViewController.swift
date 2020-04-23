7b21b9df4a1a87c4f2dea401a84d960a19717798
James McDougall <jameswiliammcdougall@gmail.com>
2020-04-22 18:24:38 -0700

    Initial Commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Lambda_School_loaner_226 on 4/22/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
