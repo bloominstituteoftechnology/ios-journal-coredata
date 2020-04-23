1ba19fcde378f4ae609d84dca152cb11c40a01d7
Cameron Collins <cameroncollins19@yahoo.com>
2020-04-22 19:39:28 -0500

    Day2 Finished

--- Journal/Journal/View Controllers/ShowEntryViewController.swift
+++ Journal/Journal/View Controllers/ShowEntryViewController.swift
@@ -16,6 +16,42 @@ class ShowEntryViewController: UIViewController {
         updateView()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        guard let entry = entry else {
+            return
+        }
+        
+        entry.title = textFieldTitle.text
+        entry.bodyText = textViewDescription.text
+        
+        var mood: MoodType?
+               
+        switch segmentedControlMood.selectedSegmentIndex {
+        case 0:
+            mood = MoodType.happy
+        case 1:
+            mood = MoodType.moderate
+        case 2:
+            mood = MoodType.unhappy
+        default:
+            mood = MoodType.moderate
+        }
+        
+        guard let newMood = mood?.rawValue else {
+            return
+        }
+        
+        entry.mood = newMood
+        
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
     //MARK: - Variables
     var entry: Entry?
     var canEdit = false
@@ -28,19 +64,41 @@ class ShowEntryViewController: UIViewController {
     
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
         canEdit = !canEdit
+        
+        if canEdit == true {
+            sender.title = "Done"
+            cancelButton.isEnabled = false
+        } else {
+            cancelButton.isEnabled = true
+            sender.title = "Edit"
+            
+            //Save Content
+            do {
+                try CoreDataStack.shared.mainContext.save()
+            } catch {
+                print("Couldn't Save: \(error)")
+            }
+        }
+         
+        textFieldTitle.isUserInteractionEnabled = canEdit
+        textViewDescription.isUserInteractionEnabled = canEdit
+        segmentedControlMood.isUserInteractionEnabled = canEdit
     }
     
     //MARK: - Outlet
     @IBOutlet weak var segmentedControlMood: UISegmentedControl!
     @IBOutlet weak var textFieldTitle: UITextField!
     @IBOutlet weak var textViewDescription: UITextView!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
     
     //MARK: - Functions
-    
     //Updates Views
     func updateView() {
         textFieldTitle.text = entry?.title
         textViewDescription.text = entry?.bodyText
+        textFieldTitle.isUserInteractionEnabled = canEdit
+        textViewDescription.isUserInteractionEnabled = canEdit
+        segmentedControlMood.isUserInteractionEnabled = canEdit
         
         var mood: Int = 0
         
@@ -57,6 +115,5 @@ class ShowEntryViewController: UIViewController {
         
         segmentedControlMood.selectedSegmentIndex = mood
     }
-
 }
 
