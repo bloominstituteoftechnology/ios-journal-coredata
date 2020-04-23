2556f0c1892ad2722444e71c63ed957c7b21cf3d
Libby Thomas <thomaslibby05@gmail.com>
2020-04-22 19:56:33 -0500

    Initial Commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Elizabeth Thomas on 4/22/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
