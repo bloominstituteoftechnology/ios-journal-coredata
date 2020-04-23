b68cf9b208fe0418fa33f4a3c5f5baf05ede478e
kensam1 <samarakodykennedy@gmail.com>
2020-04-22 23:57:35 -0400

    Journal Project

---
+++ CoreData/CoreData/CoreDataStack.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDataStack.swift
+//  CoreData
+//
+//  Created by Lambda_School_Loaner_241 on 4/22/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import Foundation
+
+
+// part 1 - file tasks - 5
---
+++ CoreData/CoreData/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  CoreData
+//
+//  Created by Lambda_School_Loaner_241 on 4/22/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
