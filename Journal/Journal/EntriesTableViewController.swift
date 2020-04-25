558e50173e5f5ef37a884abac23b0e08a62633dc
morgan smith <mssmitty07@gmail.com>
2020-04-25 16:34:59 -0400

    cant get detail segue to work

--- Journal/Journal/EntriesTableViewController.swift
+++ Journal/Journal/EntriesTableViewController.swift
@@ -122,8 +122,15 @@ class EntriesTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    if segue.identifier == "showDetail" {
+        
+        guard let indexPath = tableView.indexPathForSelectedRow, let destinationVC = segue.destination as? EntryDetailViewController else { return }
+      
+       let entry = fetchedResultsController.object(at: indexPath)
+        destinationVC.entry = entry
+        
+    }
+
     }
 
 }
--- Journal/Journal/EntryDetailViewController.swift
+++ Journal/Journal/EntryDetailViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var journalMood: UISegmentedControl!
+      
+      @IBOutlet weak var journalTitle: UITextField!
+      
+      @IBOutlet weak var journalText: UITextView!
+    
+    var entry: Entry?
+    var wasEdited: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let editButtonItem = UIBarButtonItem()
+        navigationItem.rightBarButtonItem = editButtonItem
+        updateViews()
+    }
+    
+    func updateViews() {
+        guard let entry = entry else { return }
+        
+        journalMood.selectedSegmentIndex = EntryMood.allCases.firstIndex(of: EntryMood(rawValue: entry.mood!)!) ?? 1
+        
+        journalTitle.text = entry.title
+        journalText.text = entry.bodyText
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -25,6 +46,6 @@ class EntryDetailViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
