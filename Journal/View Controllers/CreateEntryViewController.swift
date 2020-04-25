a0ee11e94587fd327ff313e988a003b37aa2c53b
Chad Parker <chad@chadparker.com>
2020-04-24 19:20:52 -0700

    Outlets and actions in CreateEntryVC, saving to Core Data

--- Journal/View Controllers/CreateEntryViewController.swift
+++ Journal/View Controllers/CreateEntryViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class CreateEntryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        guard
+            let title = titleTextField.text,
+            !title.isEmpty,
+            let body = bodyTextView.text else { return }
+            
+        Entry(title: title,
+              bodyText: body,
+              context: CoreDataStack.shared.mainContext)
+        do {
+            try CoreDataStack.shared.mainContext.save()
+            navigationController?.dismiss(animated: true, completion: nil)
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
 }
-
