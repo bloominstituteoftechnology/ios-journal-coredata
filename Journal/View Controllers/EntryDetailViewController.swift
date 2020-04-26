0685c70f00db0b314b49c2da812275e78a54d321
Chad Parker <chad@chadparker.com>
2020-04-26 15:13:25 -0700

    Add EntryDetailVC methods, edit mode, fetch and update entry, storyboard

--- Journal/View Controllers/EntryDetailViewController.swift
+++ Journal/View Controllers/EntryDetailViewController.swift
@@ -7,16 +7,83 @@
 //
 
 import UIKit
+import CoreData
 
 class EntryDetailViewController: UIViewController {
     
-    var entry: Entry? {
-        didSet {
-            print(entry ?? "")
-        }
-    }
+    var entry: Entry?
+    
+    private var wasEdited = false
+    
+    @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var moodSegmentControl: UISegmentedControl!
+    @IBOutlet private weak var bodyTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.setRightBarButton(editButtonItem, animated: false)
+        updateViews()
+    }
+    
+    private func updateViews() {
+        guard let entry = entry else { fatalError() }
+        
+        let moodString = entry.mood ?? ""
+        let mood = Mood(rawValue: moodString) ?? Mood.😐
+        let moodIndex = Mood.allCases.firstIndex(of: mood) ?? 1
+        
+        titleTextField.text = entry.title
+        moodSegmentControl.selectedSegmentIndex = moodIndex
+        bodyTextView.text = entry.bodyText
+        
+        [titleTextField, moodSegmentControl, bodyTextView].forEach {
+            $0?.isUserInteractionEnabled = isEditing
+        }
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if editing {
+            wasEdited = true
+        }
+        [titleTextField, moodSegmentControl, bodyTextView].forEach {
+            $0?.isUserInteractionEnabled = editing
+        }
+        navigationItem.hidesBackButton = editing
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if wasEdited {
+            guard
+                let entry = entry,
+                let id = entry.identifier,
+                let title = titleTextField.text,
+                let body = bodyTextView.text else { return }
+            
+            let mood = Mood.allCases[moodSegmentControl.selectedSegmentIndex]
+            
+//            Entry(title: title,
+//                  mood: mood,
+//                  bodyText: body,
+//                  context: .mainContext)
+            
+            let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "identifier == %@", id)
+            
+            do {
+                let matchingEntries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+                let existingEntry = matchingEntries[0]
+                existingEntry.title = title
+                existingEntry.mood = mood.rawValue
+                existingEntry.bodyText = body
+                try CoreDataStack.shared.mainContext.save()
+            } catch {
+                NSLog("Error saving: \(error)")
+            }
+        }
     }
 }
