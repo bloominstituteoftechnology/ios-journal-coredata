74fb60d3ffe2ee08ec04e94442f11c5dfaf69dcb
Stephanie Ballard <user.name>
2020-05-18 15:00:47 -0700

    core data stack and entry model created

--- Journal/Journal/CoreData/CoreDataStack.swift
+++ Journal/Journal/CoreData/CoreDataStack.swift
@@ -5,5 +5,25 @@
 //  Created by Stephanie Ballard on 5/18/20.
 //  Copyright © 2020 Stephanie Ballard. All rights reserved.
 //
-
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Journal")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
