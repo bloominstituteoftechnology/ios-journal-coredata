c1e4531beadc9dc0692228437090077e4c5c804c
Stephanie Ballard <user.name>
2020-05-18 16:00:42 -0700

    setting up cell

--- Journal/Journal/View/EntryTableViewCell.swift
+++ Journal/Journal/View/EntryTableViewCell.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets -
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var entryTimestampLabel: UILabel!
+    @IBOutlet weak var entryBodyTextView: UILabel!
+    
+    // MARK: - Properties -
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    static let reuseIdentifier = "EntryCell"
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +33,10 @@ class EntryTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func updateViews() {
+        
     }
 
 }
