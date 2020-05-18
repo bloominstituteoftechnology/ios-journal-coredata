4090e6d9a42ab81dfdd5310d6f8561a54eaa8151
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-05-18 12:40:38 -0700

    Added new files

---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Enzo Jimenez-Soto on 5/18/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/View Controller/CreateEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Enzo Jimenez-Soto on 5/18/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import UIKit
+
+class CreateEnteryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
