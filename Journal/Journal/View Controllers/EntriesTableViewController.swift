6bcee2ca27dd0e6d376f2ee82a23437666015ce2
Harmony Radley <harmonyradley99@gmail.com>
2020-05-19 17:23:48 -0400

    Added Prepare for segue method

--- Journal/Journal/View Controllers/EntriesTableViewController.swift
+++ Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -77,10 +77,13 @@ class EntriesTableViewController: UITableViewController {
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowEntryDetailSegue" {
+            if let detailVC = segue.destination as? EntryDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                detailVC.entry = fetchedResultsController.object(at: indexPath)
+            }
+        }
     }
 
 }
