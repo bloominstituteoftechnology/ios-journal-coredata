041011099004737116f1322e456ff83972ba7c4d
Stephanie Ballard <user.name>
2020-05-19 12:56:48 -0700

    updated initializer with the mood enum and property

--- Journal/Journal/CoreData/Entry+Convenience.swift
+++ Journal/Journal/CoreData/Entry+Convenience.swift
@@ -9,17 +9,25 @@
 import Foundation
 import CoreData
 
+enum Mood: String, CaseIterable {
+    case happy = "☀️"
+    case neutral = "⛅️"
+    case sad = "🌧"
+}
+
 extension Entry {
     
     @discardableResult convenience init(identifier: UUID = UUID(),
                                         title: String,
                                         bodyText: String,
+                                        mood: String = Mood.neutral.rawValue,
                                         timestamp: Date = Date(),
                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.identifier = identifier
         self.title = title
         self.bodyText = bodyText
+        self.mood = mood
         self.timestamp = timestamp
         
     }
