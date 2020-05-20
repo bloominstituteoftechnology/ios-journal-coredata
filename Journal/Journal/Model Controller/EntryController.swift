9ae8ca0211db948c59ea25aa6ef553a1f105fe5d
Harmony Radley <harmonyradley99@gmail.com>
2020-05-20 17:20:56 -0400

    Created deleteEntryFromServer method

--- Journal/Journal/Model Controller/EntryController.swift
+++ Journal/Journal/Model Controller/EntryController.swift
@@ -25,36 +25,57 @@ class EntryController {
     typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
     
     
-    func sendTaskToServer(entry: Entry, completion: @escaping CompletionHandler =
+    func sendEntryToServer(entry: Entry, completion: @escaping CompletionHandler =
     { _ in }) {
-    guard let uuid = entry.identifier else {
-        completion(.failure(.noIdentifier))
-        return
+        guard let uuid = entry.identifier else {
+            completion(.failure(.noIdentifier))
+            return
+        }
+        
+        let requestURL = baseURL!.appendingPathComponent(uuid.uuidString).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            guard let representation = entry.entryRepresentation else {
+                completion(.failure(.noRep))
+                return
+            }
+            request.httpBody = try JSONEncoder().encode(representation)
+        } catch {
+            NSLog("Error encoding entry \(entry): \(error)")
+            completion(.failure(.noEncode))
+            return
+        }
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                NSLog("Error sending entry to server: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            completion(.success(true))
+        }.resume()
     }
-        
-    let requestURL = baseURL!.appendingPathComponent(uuid.uuidString).appendingPathExtension("json")
-        
-    var request = URLRequest(url: requestURL)
-    request.httpMethod = "PUT"
     
-    do {
-        guard let representation = entry.entryRepresentation else {
-            completion(.failure(.noRep))
+    func deleteEntryFromServer(entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        guard let uuid = entry.identifier else {
+            completion(.failure(.noIdentifier))
             return
         }
-        request.httpBody = try JSONEncoder().encode(representation)
-    } catch {
-        NSLog("Error encoding entry \(entry): \(error)")
-        completion(.failure(.noEncode))
-        return
+        let requestURL = baseURL!.appendingPathComponent(uuid.uuidString).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { _, _, error in
+            if let error = error {
+                NSLog("Error deleting entry from server: \(entry): \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            completion(.success(true))
+        }.resume()
     }
-    URLSession.shared.dataTask(with: request) { data, response, error in
-        if let error = error {
-            NSLog("Error sending entry to server: \(error)")
-            completion(.failure(.otherError))
-            return
-        }
-        completion(.success(true))
-    }.resume()
 }
-}
+
