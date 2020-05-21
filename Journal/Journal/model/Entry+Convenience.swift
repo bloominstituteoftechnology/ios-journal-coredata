6d5de963fe6a2f7af40d790ad0d734770be10c07
Vincent Hoang <vietnhoang7@gmail.com>
2020-05-20 23:23:38 -0700

    Continue day3 progress

--- Journal/Journal/model/Entry+Convenience.swift
+++ Journal/Journal/model/Entry+Convenience.swift
@@ -16,6 +16,20 @@ enum Mood: String, CaseIterable {
 }
 
 extension Entry {
+    var entryRepresentation: EntryRepresentation? {
+        guard let id = identifier,
+            let title = title,
+            let timeStamp = timeStamp,
+            let mood = mood,
+            let bodyText = bodyText else { return nil }
+        
+        return EntryRepresentation(identifier: id.uuidString,
+                                   title: title,
+                                   bodyText: bodyText,
+                                   mood: mood,
+                                   timeStamp: timeStamp)
+    }
+    
     @discardableResult convenience init(identifier: UUID = UUID(),
                                         title: String,
                                         bodyText: String,
@@ -28,5 +42,17 @@ extension Entry {
         self.bodyText = bodyText
         self.timeStamp = timeStamp
         self.mood = mood.rawValue
+    }
+    
+    @discardableResult convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let identifier = UUID(uuidString: entryRepresentation.identifier),
+            let mood = Mood(rawValue: entryRepresentation.mood) else { return nil }
+        
+        self.init(identifier: identifier,
+                  title: entryRepresentation.title,
+                  bodyText: entryRepresentation.bodyText,
+                  timeStamp: entryRepresentation.timeStamp,
+                  mood: mood,
+                  context: context)
     }
 }
--- Journal/Journal/modelcontroller/EntryController.swift
+++ Journal/Journal/modelcontroller/EntryController.swift
@@ -18,5 +18,66 @@ enum NetworkError: Error {
 }
 
 class EntryController {
+    private let baseURL: URL = URL(string: "https://journal-1f869.firebaseio.com/")!
     
+    func sendEntryToServer(_ entry: Entry, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { data, _, error in
+            if let error = error {
+                NSLog("Error fetching tasks: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from Firebase (fetching entries).")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let entryRepresentations = Array(try JSONDecoder().decode([String : EntryRepresentation].self, from: data).values)
+                try self.updateEntries(with: entryRepresentations)
+            } catch {
+                NSLog("Error decoding entries from Firebase: \(error)")
+                completion(.failure(.failedDecode))
+            }
+        }
+    }
+    
+    private func updateEntries(with representations: [EntryRepresentation]) throws {
+        let identifiersToFetch = representations.compactMap { UUID(uuidString: $0.identifier) }
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        var entriesToCreate = representationsByID
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+        
+        let context = CoreDataStack.shared.mainContext
+        
+        let existingEntries = try context.fetch(fetchRequest)
+        
+        for entry in existingEntries {
+            guard let id = entry.identifier, let representation = representationsByID[id] else {
+                continue
+            }
+            
+            self.update(entry: entry, with: representation)
+            entriesToCreate.removeValue(forKey: id)
+        }
+        
+        for representation in entriesToCreate.values {
+            Entry(entryRepresentation: representation, context: context)
+        }
+        
+        try context.save()
+    }
+    
+    private func update(entry: Entry, with representation: EntryRepresentation) {
+        entry.title = representation.title
+        entry.timeStamp = representation.timeStamp
+        entry.bodyText = representation.bodyText
+        entry.mood = representation.mood
+    }
 }
