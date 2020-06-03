39501d8625a3f0cf750684730e311f8833eb6836
Clayton Watkins <watkinclayton17@yahoo.com>
2020-06-03 13:28:59 -0700

    Initial Commit. Set up MVC File structure, and started CDS.

---
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Clayton Watkins on 6/3/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Journal/ViewControllers/CreateEntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateEntryViewController.swift
+//  Journal
+//
+//  Created by Clayton Watkins on 6/3/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class CreateEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
