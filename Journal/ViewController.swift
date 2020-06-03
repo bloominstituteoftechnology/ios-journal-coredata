f440288a623f2950b7cd38c319d281f8d4719ab0
Kenneth Jones <kennethjones1991@outlook.com>
2020-06-03 10:27:09 -0600

    Initial commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Kenneth Jones on 6/3/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
