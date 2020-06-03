e125cf6d5acec53aa3395a510cb244e454b5f382
Kenneth Jones <kennethjones1991@outlook.com>
2020-06-03 11:17:07 -0600

    Completed Part 2

--- Journal/Entry+Convenience.swift
+++ Journal/Entry+Convenience.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import CoreData
+
+extension Entry {
+    @discardableResult convenience init(identifier: UUID = UUID(),
+                                        title: String,
+                                        bodyText: String? = nil,
+                                        timestamp: Date = Date(),
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.identifier = identifier
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+    }
+}
