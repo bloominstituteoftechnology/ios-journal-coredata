14c3e74ae7d418741e71d381653ad6be0ce6c0cc
Kenneth Jones <kennethjones1991@outlook.com>
2020-06-03 11:06:30 -0600

    Completed Part 1

---
+++ Journal/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Kenneth Jones on 6/3/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Kenneth Jones on 6/3/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
