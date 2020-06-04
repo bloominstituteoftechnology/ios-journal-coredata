571bfea5ac4ade7f60a8f12b4be03b3a8b37199c
morgan smith <mssmitty07@gmail.com>
2020-06-04 18:39:46 -0400

    fixed title issue

--- Journal/Journal/CreateEntryViewController.swift
+++ Journal/Journal/CreateEntryViewController.swift
@@ -31,7 +31,7 @@ class CreateEntryViewController: UIViewController {
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
     
         // Grab the individual values from the views
-           guard let title = journalText.text, !title.isEmpty else { return }
+           guard let title = journalTitle.text, !title.isEmpty else { return }
            
         guard let bodyText = journalText.text, !bodyText.isEmpty else { return }
           
