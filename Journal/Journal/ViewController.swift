9f6336afd9a2ba599eb70d48d0513d8b034da491
Bronson <bronsonmullens@icloud.com>
2020-06-03 19:09:52 -0700

    Initial commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Bronson Mullens on 6/3/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
