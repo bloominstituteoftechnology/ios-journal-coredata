188f155618633c1f4fc72121592602f0bdd47d1a
Clayton Watkins <watkinclayton17@yahoo.com>
2020-06-10 12:39:53 -0700

    Added ability to update and sync with database

--- Journal/Journal/Model Controller/EntryController.swift
+++ Journal/Journal/Model Controller/EntryController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 // To handle network call errors
 enum NetworkError: Error{
@@ -64,6 +65,7 @@ class EntryController {
         }.resume()
     }
     
+    // This allows us to delete our entry from the database
     func deleteEntryFromServer(entry: Entry, completion: @escaping CompletionHandler = { _ in }){
         guard let uuid = entry.identifier else {
             completion(.failure(.noIdentifier))
@@ -81,5 +83,53 @@ class EntryController {
         }.resume()
     }
     
+    
+    
+    // MARK: - Private Functions
+    
+    // Here we are setting our entries properties to a representation so that we can use it to check if we are reciving unique values from the server, or if they already exist in our persistent store
+    private func update(entry: Entry, with representation: EntryRepresentation){
+        entry.title = representation.title
+        entry.bodyText = representation.bodyText
+        entry.timestamp = representation.timestamp
+        entry.mood = representation.mood
+    }
+    
+    private func saveToPersistentStore() throws{
+        let moc = CoreDataStack.shared.mainContext
+        try moc.save()
+    }
+        
+    private func updateTasks(with representations: [EntryRepresentation]) throws {
+        
+        // Creating an array of UUIDs
+        let identifiersToFetch = representations.compactMap({ $0.identifier })
+        // Creating Dictionaries of our representations by create a key value pair of UUID : representation
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        var tasksToCreate = representationsByID
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifer IN %@", identifiersToFetch)
+        
+        let context = CoreDataStack.shared.mainContext
+        do{
+            let existingTasks = try context.fetch(fetchRequest)
+            
+            // For already existing tasks
+            for entry in existingTasks{
+                guard let id = entry.identifier,
+                    let representation = representationsByID[id] else { continue }
+                // Update Task
+                self.update(entry: entry, with: representation)
+                tasksToCreate.removeValue(forKey: id)
+            }
+            
+            for representation in tasksToCreate.values{
+                Entry(entryRepresentation: representation, context: context)
+            }
+        } catch {
+            print("Error fetching tasks for UUIDs: \(error)")
+        }
+        try self.saveToPersistentStore()
+    }
     
 }
