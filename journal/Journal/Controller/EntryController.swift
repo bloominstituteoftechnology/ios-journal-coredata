2181f13fc2516184a40be6728ea11797db06740e
Ian French <61711015+IanFrench@users.noreply.github.com>
2020-06-14 17:58:19 -0400

    Day 4
    @DeVitoC.  App functions in simulator, but still getting decode error.

---
+++ journal/Journal/Controller/EntryController.swift
@@ -0,0 +1,198 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Ian French on 6/11/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum NetworkError: Error {
+    case noIdentifier
+    case otherError
+    case noData
+    case noDecode
+    case noEncode
+    case noRep
+}
+
+class EntryController {
+    
+    init() {
+        fetchEntriesFromServer()
+    }
+    
+    typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
+    
+    // MARK: - Properties
+    let baseURL = URL(string: "https://lambdajournalday3.firebaseio.com/")!
+    
+    // MARK: - Functions
+    
+    
+    func fetchEntriesFromServer(completion: @escaping CompletionHandler = { _ in }) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching tasks: \(error)")
+                DispatchQueue.main.async {
+                    completion(.failure(.otherError))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error: No data returned from data task")
+                DispatchQueue.main.async {
+                    completion(.failure(.noData))
+                }
+                return
+            }
+            
+
+            var entryRepresentations: [EntryRepresentation] = []
+
+            do {
+
+                entryRepresentations = try JSONDecoder().decode([String : EntryRepresentation].self, from: data).map({ $0.value})
+                
+                try self.updateEntries(with: entryRepresentations)
+                
+                DispatchQueue.main.async {
+                    completion(.success(true))
+                }
+            } catch {
+                NSLog("Error decoding task representations: \(error)")
+                DispatchQueue.main.async {
+                    completion(.failure(.noDecode))
+                }
+            }
+        }.resume()
+        
+        
+    }
+    
+    
+    
+    
+    
+    func sendEntryToServer(entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        
+        guard let identifier = entry.identifier else {
+            completion(.failure(.noIdentifier))
+            return
+        }
+        
+        
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            guard let representation = entry.entryRepresentation else {
+                completion(.failure(.noRep))
+                return
+            }
+            request.httpBody = try JSONEncoder().encode(representation)
+        } catch {
+            print("Error encoding task \(entry): \(error)")
+            completion(.failure(.noEncode))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error PUTting entry to server: \(error)")
+                DispatchQueue.main.async {
+                    completion(.failure(.otherError))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(true))
+            }
+        }.resume()
+    }
+    
+    func updateEntries(with representations: [EntryRepresentation]) throws {
+        let context = CoreDataStack.shared.container.newBackgroundContext()
+        let identifiersToFetch = representations.compactMap({ $0.identifier })
+
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        var entriesToCreate = representationsByID
+
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+
+        context.performAndWait {
+            do {
+
+                let existingEntries = try context.fetch(fetchRequest)
+
+                // for existing entries
+                for entry in existingEntries {
+                    guard let id = entry.identifier,
+                        let representation = representationsByID[id] else { continue }
+
+                    update(entry: entry, entryRepresentation:  representation)
+
+                    entriesToCreate.removeValue(forKey: id)
+                }
+
+                // for new entries
+                for representation in entriesToCreate.values {
+                    Entry(entryRepresentation: representation, context: context)
+                }
+
+            } catch {
+                print("Error fetching tasks for Identifier: \(error)")
+            }
+
+            do {
+
+                try CoreDataStack.shared.save(context:context)
+            } catch {
+                print("error saving")
+            }
+        }
+    }
+    
+    
+    
+    func deleteEntryFromServer(entry: Entry, completion: @escaping CompletionHandler = { _ in }) {
+        guard let identifier = entry.identifier else {
+            completion(.failure(.noIdentifier ))
+            return
+        }
+        
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(response!)
+            DispatchQueue.main.async {
+                completion(.success(true))
+            }
+        }.resume()
+    }
+    
+    private func update(entry: Entry, entryRepresentation: EntryRepresentation) {
+        entry.title = entryRepresentation.title
+        entry.bodyText = entryRepresentation.bodyText
+        entry.mood = entryRepresentation.mood
+        entry.timestamp = entryRepresentation.timestamp
+    }
+}
+
+
---
+++ journal/Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -0,0 +1,208 @@
+//
+//  EntriesTableViewController.swift
+//  Journal
+//
+//  Created by Ian French on 6/3/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class EntriesTableViewController: UITableViewController {
+
+
+private let entryController = EntryController()
+
+    lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [
+        NSSortDescriptor(key: "mood", ascending: true),
+        NSSortDescriptor(key: "timestamp", ascending: true)
+        ]
+        
+        let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "mood", cacheName: nil)
+        
+        frc.delegate = self
+        do {
+                   try frc.performFetch()
+               } catch {
+                   NSLog("Error fetching Entry objects")
+               }
+               return frc
+           }()
+  
+    
+    
+    
+    /*
+    var entries: [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        let context = CoreDataStack.shared.mainContext
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching entries: \(error)")
+            return []
+        }
+    }
+    
+   */
+    
+    
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    
+      override func numberOfSections(in tableView: UITableView) -> Int {
+         return fetchedResultsController.sections?.count ?? 1
+     }
+
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         
+         return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+     }
+     
+
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         guard let cell = tableView.dequeueReusableCell(withIdentifier: EntryTableViewCell.reuseIdentifier, for: indexPath) as? EntryTableViewCell else {
+             fatalError("Can't dequeue cell of type \(EntryTableViewCell.reuseIdentifier)")
+         }
+
+         cell.entry = fetchedResultsController.object(at: indexPath)
+
+         return cell
+     }
+     
+     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+         guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil }
+         return sectionInfo.name.capitalized
+     }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
+    // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+           if editingStyle == .delete {
+               let entry = fetchedResultsController.object(at: indexPath)
+            entryController.deleteEntryFromServer(entry: entry) { (result) in
+                   guard let _ = try? result.get() else { return }
+                   DispatchQueue.main.async {
+                       let moc = CoreDataStack.shared.mainContext
+                       moc.delete(entry)
+                       do {
+                           try moc.save()
+                           tableView.reloadData()
+                       } catch {
+                           moc.reset()
+                           NSLog("Error saving managed object context: \(error)")
+                       }
+                   }
+               }
+           }
+       }
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+   
+     // MARK: - Navigation
+     
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "ShowEntryDetail" {
+                if let detailVC = segue.destination as? EntryDetailViewController,
+                    let indexPath = tableView.indexPathForSelectedRow {
+                    detailVC.entry = fetchedResultsController.object(at: indexPath)
+
+                } else if segue.identifier == "CreateEntrySegue" {
+                    if let naviC = segue.destination as? UINavigationController,
+                       let createEntry = naviC.viewControllers.first as? CreateEntryViewController {
+                        createEntry.entryController = entryController
+                    }
+
+                }
+            }
+        }
+}
+
+
+
+extension EntriesTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+            let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
+}
