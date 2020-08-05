84488f49a7c1515c3fa86a8b6642b49ded62ea13
BrysonSaclausa <brysonSaclausa@Owners-MacBook-Pro.local>
2020-08-05 16:48:20 -0700

    createdProjectJournal

---
+++ iosJournalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosJournalProject
+//
+//  Created by BrysonSaclausa on 8/5/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
