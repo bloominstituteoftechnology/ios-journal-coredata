8ce795c8667b4785174bfbdc31acd21d1c254943
Gladymir <gaethan0815@gmail.com>
2020-08-05 21:48:15 -0600

    Part 1

---
+++ Journals/Journals/Models/CoreDataStack.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDataStack.swift
+//  Journals
+//
+//  Created by Gladymir Philippe on 8/5/20.
+//  Copyright © 2020 Gladymir Philippe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Journals")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
