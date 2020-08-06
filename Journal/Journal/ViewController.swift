2a8dba8e79bd6657f2ed1a126a456427d617adfd
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-08-05 17:59:49 -0700

    Started Project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Cora Jacobson on 8/3/20.
+//  Copyright © 2020 Cora Jacobson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
