86ebbfe5af0a06a3ed26e06d19d9f4dc7e5cb4d2
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-08-05 19:42:42 -0700

    View & ViewController

--- Journal/Journal/ViewControllers/CreateEntryViewController.swift
+++ Journal/Journal/ViewControllers/CreateEntryViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class CreateEntryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text,
+            !title.isEmpty,
+            let bodyText = bodyTextView.text,
+            !bodyText.isEmpty else { return }
+        Entry(title: title, bodyText: bodyText)
+        do {
+            try CoreDataStack.shared.mainContext.save()
+            navigationController?.dismiss(animated: true, completion: nil)
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
 }
 
--- Journal/Journal/Views/EntryTableViewCell.swift
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
 class EntryTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - Properties
+    
+    static let reuseIdentifier = "EntryCell"
+    private let formatter = DateFormatter()
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    
+    
+    // MARK: - Private Functions
+    
+    private func updateViews() {
+        guard let entry = entry else { return }
+        titleLabel.text = entry.title
+        bodyTextLabel.text = entry.bodyText
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        if let timestamp = entry.timestamp {
+            timestampLabel.text = formatter.string(from: timestamp)
+        }
     }
 
 }
