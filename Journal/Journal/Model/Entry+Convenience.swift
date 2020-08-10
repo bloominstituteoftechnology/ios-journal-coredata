e9f4e8c50e24b57d56673fde8bdd1eaf861d3e8f
Craig Belinfante <craigbasketball@aol.com>
2020-08-10 09:02:20 -0700

    working on storyboard still. running into issues. have to fix convenience model.

--- Journal/Journal/Model/Entry+Convenience.swift
+++ Journal/Journal/Model/Entry+Convenience.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import CoreData
+
+@objc(Entry)
+public class Entry: NSManagedObject {
+
+}
+
+extension Entry {
+    
+    @discardableResult convenience init(UIidentifier: UUID = UUID(),
+                                        title: String,
+                                        bodyText: String,
+                                        timestamp: Date,
+                                        identifier: String,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.UIidentifier = UIidentifier
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
+
+
